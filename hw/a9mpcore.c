5338ae0424e7db26ede20f8b1c3c8f07f329f32d
Peter Crosthwaite <peter.crosthwaite@xilinx.com>
2012-11-26 10:15:52 +1000

    arm: mptimer: Remove WDT distinction
    In QEMU emulation, there is no functional difference between the ARM mpcore
    private timers and watchdogs. Removed all the distinction between the two from
    arm_mptimer.c and converted it to be just the mptimer. a9mpcore and arm11mpcore
    just instantiate the same mptimer object twice to get both timer and WDT.
    
    If in the future we want to make the WDT functionally different then we can use
    either QOM heirachy to derive WDT from from mptimer, or we can add a property
    "is-wdt" or some such.
    
    Signed-off-by: Peter Crosthwaite <peter.crosthwaite@xilinx.com>

--- hw/a9mpcore.c
+++ hw/a9mpcore.c
@@ -128,7 +128,7 @@ static void a9mp_priv_set_irq(void *opaque, int irq, int level)
 static int a9mp_priv_init(SysBusDevice *dev)
 {
     A9MPPrivState *s = FROM_SYSBUS(A9MPPrivState, dev);
-    SysBusDevice *busdev, *gicbusdev;
+    SysBusDevice *timerbusdev, *wdtbusdev, *gicbusdev;
     DeviceState *qdev;
     int i;
 
@@ -147,7 +147,12 @@ static int a9mp_priv_init(SysBusDevice *dev)
     qdev = qdev_create(NULL, "arm_mptimer");
     qdev_prop_set_uint32(qdev, "num-cpu", s->num_cpu);
     qdev_init_nofail(qdev);
-    busdev = sysbus_from_qdev(qdev);
+    timerbusdev = sysbus_from_qdev(qdev);
+
+    qdev = qdev_create(NULL, "arm_mptimer");
+    qdev_prop_set_uint32(qdev, "num-cpu", s->num_cpu);
+    qdev_init_nofail(qdev);
+    wdtbusdev = sysbus_from_qdev(qdev);
 
     /* Memory map (addresses are offsets from PERIPHBASE):
      *  0x0000-0x00ff -- Snoop Control Unit
@@ -170,9 +175,9 @@ static int a9mp_priv_init(SysBusDevice *dev)
      * memory region, not the "timer/watchdog for core X" ones 11MPcore has.
      */
     memory_region_add_subregion(&s->container, 0x600,
-                                sysbus_mmio_get_region(busdev, 0));
+                                sysbus_mmio_get_region(timerbusdev, 0));
     memory_region_add_subregion(&s->container, 0x620,
-                                sysbus_mmio_get_region(busdev, 1));
+                                sysbus_mmio_get_region(wdtbusdev, 0));
     memory_region_add_subregion(&s->container, 0x1000,
                                 sysbus_mmio_get_region(gicbusdev, 0));
 
@@ -183,9 +188,9 @@ static int a9mp_priv_init(SysBusDevice *dev)
      */
     for (i = 0; i < s->num_cpu; i++) {
         int ppibase = (s->num_irq - 32) + i * 32;
-        sysbus_connect_irq(busdev, i * 2,
+        sysbus_connect_irq(timerbusdev, i,
                            qdev_get_gpio_in(s->gic, ppibase + 29));
-        sysbus_connect_irq(busdev, i * 2 + 1,
+        sysbus_connect_irq(wdtbusdev, i,
                            qdev_get_gpio_in(s->gic, ppibase + 30));
     }
     return 0;
