2fc62e77c4e78c478dcad9fedbe8e1ce1d11e018
Peter Crosthwaite <peter.crosthwaite@xilinx.com>
2013-04-11 16:59:29 +1000

    Advance of linear history for update of osl/pub/fdt-generic.devl to commit c32452a0683c1a48592b27f885f65b79335fd010
    Signed-off-by: Peter Crosthwaite <peter.crosthwaite@xilinx.com>

--- hw/fdt_generic_devices.h
+++
@@ -1,8 +0,0 @@
-#ifndef FDT_GENERIC_DEVICES_H
-#define FDT_GENERIC_DEVICES_H
-
-#include "fdt_generic.h"
-
-int pflash_cfi01_fdt_init(char *node_path, FDTMachineInfo *fdti, void *opaque);
-
-#endif /* FDT_GENERIC_DEVICES_H */
