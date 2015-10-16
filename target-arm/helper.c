a1d46cda370f33b212e88da32374d73425d86bb2
Davorin Mista <davorin.mista@aggios.com>
2015-10-16 11:14:53 +0100

    target-arm: Implement AArch64 OSLAR/OSLSR_EL1 sysregs
    Added oslar_write function to OSLAR_EL1 sysreg, using a status variable
    in ARMCPUState.cp15 struct (oslsr_el1). This variable is also linked
    to the newly added read-only OSLSR_EL1 register.
    
    Linux reads from this register during its suspend/resume procedure.
    
    Signed-off-by: Davorin Mista <davorin.mista@aggios.com>
    [PMM: folded a long line and tweaked a comment]
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    [ Changes by AF:
     - Apply to Xilinx Tree
    ]
    Reviewed-by: Alistair Francis <alistair.francis@xilinx.com>

--- target-arm/helper.c
+++ target-arm/helper.c
@@ -3167,6 +3167,23 @@ static void dcc_write(CPUARMState *env, const ARMCPRegInfo *ri,
     putchar(value);
 }
 
+static void oslar_write(CPUARMState *env, const ARMCPRegInfo *ri,
+                        uint64_t value)
+{
+    /* Writes to OSLAR_EL1 may update the OS lock status, which can be
+     * read via a bit in OSLSR_EL1.
+     */
+    int oslock;
+
+    if (ri->state == ARM_CP_STATE_AA32) {
+        oslock = (value == 0xC5ACCE55);
+    } else {
+        oslock = value & 1;
+    }
+
+    env->cp15.oslsr_el1 = deposit32(env->cp15.oslsr_el1, 1, 1, oslock);
+}
+
 static const ARMCPRegInfo debug_cp_reginfo[] = {
     /* DBGDRAR, DBGDSAR: always RAZ since we don't implement memory mapped
      * debug components. The AArch64 version of DBGDRAR is named MDRAR_EL1;
@@ -3194,12 +3211,15 @@ static const ARMCPRegInfo debug_cp_reginfo[] = {
       .cp = 14, .opc0 = 2, .opc1 = 0, .crn = 0, .crm = 1, .opc2 = 0,
       .type = ARM_CP_NO_MIGRATE,
       .access = PL1_R,
-      .fieldoffset = offsetof(CPUARMState, cp15.mdscr_el1),
-      .resetfn = arm_cp_reset_ignore },
-    /* We define a dummy WI OSLAR_EL1, because Linux writes to it. */
+      .fieldoffset = offsetof(CPUARMState, cp15.mdscr_el1), },
     { .name = "OSLAR_EL1", .state = ARM_CP_STATE_BOTH,
       .cp = 14, .opc0 = 2, .opc1 = 0, .crn = 1, .crm = 0, .opc2 = 4,
-      .access = PL1_W, .type = ARM_CP_NOP },
+      .access = PL1_W, .type = ARM_CP_STATE_BOTH,
+      .writefn = oslar_write },
+    { .name = "OSLSR_EL1", .state = ARM_CP_STATE_BOTH,
+      .cp = 14, .opc0 = 2, .opc1 = 0, .crn = 1, .crm = 1, .opc2 = 4,
+      .access = PL1_R, .resetvalue = 10,
+      .fieldoffset = offsetof(CPUARMState, cp15.oslsr_el1) },
     /* Dummy OSDLR_EL1: 32-bit Linux will read this */
     { .name = "OSDLR_EL1", .state = ARM_CP_STATE_BOTH,
       .cp = 14, .opc0 = 2, .opc1 = 0, .crn = 1, .crm = 3, .opc2 = 4,
