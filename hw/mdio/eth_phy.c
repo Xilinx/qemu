2cd9a4c8a1d758f87bbe6b05ae02156b8727db8a
Sai Pavan Boddu <sai.pavan.boddu@xilinx.com>
2016-02-22 05:02:00 -0800

    mdio: Add Ethernet PHY model
    This is a basic Ethernet Phy model which indicates the link is up on reset.
    There is initial support for Marvel and TI models.
    
    Signed-off-by: Sai Pavan Boddu <saipava@xilinx.com>
    Reviewed-by: Alistair Francis <alistair.francis@xilinx.com>

---
+++ hw/mdio/eth_phy.c
@@ -0,0 +1,197 @@
+/*
+ * QEMU Ethernet MDIO bus & PHY models
+ *
+ * Copyright (c) 2008 Edgar E. Iglesias (edgari@xilinx.com),
+ * Copyright (c) 2008 Grant Likely (grant.likely@secretlab.ca),
+ * Copyright (c) 2016 Xilinx Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+#include "hw/mdio/mdio_slave.h"
+#include "hw/mdio/eth_phy.h"
+
+#ifndef ETH_PHY_DEBUG
+#define ETH_PHY_DEBUG 0
+#endif
+
+#define DPRINT(fmt, args...) \
+    do { \
+        if (ETH_PHY_DEBUG) { \
+            qemu_log("%s: " fmt, __func__, ## args); \
+        } \
+    } while (0)
+
+static void eth_phy_reset(DeviceState *dev)
+{
+    EthPhy *s = ETHPHY(dev);
+
+    /* If AutoNegotiation is supported */
+    if (s->part->autoneg) {
+        /* Show as AutoNeg capable & show as its completed autoneg */
+        s->regs[PHY_STATUS] |= M(PHY_STAT_AUTONEG_CAP) |
+                               M(PHY_STAT_AUTONEG_COMP) |
+                               M(PHY_STAT_EXT_CAP);
+
+        s->regs[PHY_CTRL] |= M(PHY_CTRLREG_AUTONEG_EN);
+        /* Supports IEEE 802.3 std and 10BaseT and 10BaseTX Full and Half
+         *  duplex
+         */
+        s->regs[PHY_AUTONEG_ADV] |= 0x01E1;
+        s->regs[PHY_LP_ABILITY]  |= 0xCDE1;
+
+        s->regs[PHY_1000T_CTRL] |= 0x0300;
+        s->regs[PHY_1000T_STATUS] |= 0x7C00;
+
+        /* Support all modes in gmii mode*/
+        if (s->part->gmii) {
+            s->regs[PHY_EXT_STATUS] |=  M(PHY_EXT_STAT_1000BT_HD) |
+                                        M(PHY_EXT_STAT_1000BT_FD) |
+                                        M(PHY_EXT_STAT_1000BX_HD) |
+                                        M(PHY_EXT_STAT_1000BX_FD);
+            s->regs[PHY_STATUS] |= M(PHY_STAT_100BX_FD) |
+                                   M(PHY_STAT_100BX_HD) |
+                                   M(PHY_STAT_100B_T2_FD) |
+                                   M(PHY_STAT_100B_T2_HD) |
+                                   M(PHY_STAT_10MBPS_HD) |
+                                   M(PHY_STAT_10MBPS_FD);
+            /* Show 1000Mb/s as default */
+            s->regs[PHY_CTRL] |= M(PHY_CTRLREG_SPEED_SEL_MSB);
+
+            /* Supports Extended status */
+            s->regs[PHY_STATUS] |= M(PHY_STAT_EXT_STAT_CAP);
+            s->regs[PHY_SPEC_STATUS] |= 0xBC00;
+        } else {
+            s->regs[PHY_STATUS] |= M(PHY_STAT_100BX_FD) |
+                                   M(PHY_STAT_100BX_HD) |
+                                   M(PHY_STAT_100B_T2_FD) |
+                                   M(PHY_STAT_100B_T2_HD) |
+                                   M(PHY_STAT_10MBPS_HD) |
+                                   M(PHY_STAT_10MBPS_FD);
+            /* Show 100Mb/s as default */
+            s->regs[PHY_CTRL] |= M(PHY_CTRLREG_SPEED_SEL_LSB);
+            s->regs[PHY_SPEC_STATUS] |= 0x7C00;
+        }
+
+    }
+    s->link = true;
+    s->regs[PHY_STATUS] |= M(PHY_STAT_LINK_STAT);
+}
+
+static int eth_phy_read(MDIOSlave *slave, uint8_t req)
+{
+    EthPhy *phy = ETHPHY(slave);
+    int regnum;
+    unsigned r = 0;
+
+    regnum = req & 0x1f;
+
+    switch (regnum) {
+    case PHY_STATUS:
+        if (!phy->link) {
+            break;
+        }
+        r = phy->regs[PHY_STATUS];
+        break;
+    default:
+        r = phy->regs[regnum];
+        break;
+    }
+    DPRINT("%s %x = reg[%d]\r\n", __func__, r, regnum);
+    return r;
+}
+
+static int eth_phy_write(MDIOSlave *slave, uint8_t req, uint8_t data)
+{
+    EthPhy *phy = ETHPHY(slave);
+    int regnum = req & 0x1f;
+    uint16_t mask = phy->regs_readonly_mask[regnum];
+
+    DPRINT("%s reg[%d] = %x; mask=%x\n", __func__, regnum, data, mask);
+    switch (regnum) {
+    case PHY_CTRL:
+        /* Update Registers on Fall through */
+        if (data & PHY_CTRL_RST) {
+            eth_phy_reset(DEVICE(phy));
+        }
+    default:
+        phy->regs[regnum] = (phy->regs[regnum] & mask) | (data & ~mask);
+        break;
+    }
+    return 0;
+}
+
+static void eth_phy_init(Object *Obj)
+{
+    EthPhy *s = ETHPHY(Obj);
+    EthPhyClass *k = ETHPHY_GET_CLASS(Obj);
+
+    s->part = k->part;
+    /* PHY Id. */
+    s->regs[PHY_ID1] = s->part->phy_id1;
+    s->regs[PHY_ID2] = s->part->phy_id2;
+
+    s->regs_readonly_mask = default_readonly_mask;
+}
+
+static void eth_phy_class_init(ObjectClass *klass, void *data)
+{
+    MDIOSlaveClass *sc = MDIO_SLAVE_CLASS(klass);
+
+    sc->send = eth_phy_write;
+    sc->recv = eth_phy_read;
+}
+
+static void phy_class_init(ObjectClass *klass, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(klass);
+    EthPhyClass *k = ETHPHY_CLASS(klass);
+
+    k->part = data;
+    dc->reset = eth_phy_reset;
+}
+
+static const TypeInfo eth_phy_info = {
+    .name = TYPE_ETH_PHY,
+    .parent = TYPE_MDIO_SLAVE,
+    .instance_size = sizeof(EthPhy),
+    .class_size = sizeof(EthPhyClass),
+    .instance_init = eth_phy_init,
+    .class_init = eth_phy_class_init,
+};
+
+static void eth_phy_register_types(void)
+{
+    int i;
+
+    type_register_static(&eth_phy_info);
+
+    for (i = 0; i < ARRAY_SIZE(devices); i++) {
+        TypeInfo ti = {
+            .name       = devices[i].partname,
+            .parent     = TYPE_ETH_PHY,
+            .class_init = phy_class_init,
+            .class_data = (void *)&devices[i],
+        };
+        type_register(&ti);
+    }
+}
+
+type_init(eth_phy_register_types)
---
+++ include/hw/mdio/eth_phy.h
@@ -0,0 +1,157 @@
+/*
+ * QEMU Ethernet MDIO bus & PHY models
+ *
+ * Copyright (c) 2008 Edgar E. Iglesias (edgari@xilinx.com),
+ *                          Grant Likely (grant.likely@secretlab.ca),
+ * Copyright (c) 2016 Xilinx Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+#ifndef ETH_PHY_H
+#define ETH_PHY_H
+
+#include "qemu-common.h"
+
+/* PHY MII Register/Bit Definitions */
+/* PHY Registers defined by IEEE */
+#define PHY_CTRL         0x00 /* Control Register */
+#define PHY_STATUS       0x01 /* Status Regiser */
+#define PHY_ID1          0x02 /* Phy Id Reg (word 1) */
+#define PHY_ID2          0x03 /* Phy Id Reg (word 2) */
+#define PHY_AUTONEG_ADV  0x04 /* Autoneg Advertisement */
+#define PHY_LP_ABILITY   0x05 /* Link Partner Ability (Base Page) */
+#define PHY_AUTONEG_EXP  0x06 /* Autoneg Expansion Reg */
+#define PHY_NEXT_PAGE_TX 0x07 /* Next Page TX */
+#define PHY_LP_NEXT_PAGE 0x08 /* Link Partner Next Page */
+#define PHY_1000T_CTRL   0x09 /* 1000Base-T Control Reg */
+#define PHY_1000T_STATUS 0x0A /* 1000Base-T Status Reg */
+#define PHY_EXT_STATUS   0x0F /* Extended Status Reg */
+#define PHY_SPEC_CTRL    0x10 /* PHY Specific control reg */
+#define PHY_SPEC_STATUS  0x11 /* PHY Specific status reg */
+#define NUM_PHY_REGS     0x1F  /* 5 bit address bus (0-0x1F) */
+
+/*Control Register bitfeild offsets*/
+#define PHY_CTRL_REG_UNIDIR_EN      5
+#define PHY_CTRLREG_SPEED_SEL_MSB   6
+#define PHY_CTRLREG_COLLISION_TEST  7
+#define PHY_CTRLREG_DUPLEX_MODE     8
+#define PHY_CTRLREG_RST_AUTONEG     9
+#define PHY_CTRLREG_ISOLATE         10
+#define PHY_CTRLREG_POWER_DWN       11
+#define PHY_CTRLREG_AUTONEG_EN      12
+#define PHY_CTRLREG_SPEED_SEL_LSB   13
+#define PHY_CTRLREG_LOOPBACK        14
+#define PHY_CTRLREG_RESET           15
+
+/*Status Register bitfeild offsets */
+#define PHY_STAT_EXT_CAP        0
+#define PHY_STAT_JAB_DETECT     1
+#define PHY_STAT_LINK_STAT      2
+#define PHY_STAT_AUTONEG_CAP    3
+#define PHY_STAT_REMOTE_FAL     4
+#define PHY_STAT_AUTONEG_COMP   5
+#define PHY_STAT_PREM_SUPPRESS  6
+#define PHY_STAT_UNIDIR_CAP     7
+#define PHY_STAT_EXT_STAT_CAP   8
+#define PHY_STAT_100B_T2_HD     9
+#define PHY_STAT_100B_T2_FD     10
+#define PHY_STAT_10MBPS_HD      11
+#define PHY_STAT_10MBPS_FD      12
+#define PHY_STAT_100BX_HD       13
+#define PHY_STAT_100BX_FD       14
+#define PHY_STAT_T4             15
+
+/* EXT Status Register bitfeild offsets*/
+#define PHY_EXT_STAT_1000BT_HD  12
+#define PHY_EXT_STAT_1000BT_FD  13
+#define PHY_EXT_STAT_1000BX_HD  14
+#define PHY_EXT_STAT_1000BX_FD  15
+
+/* Prepare a bit mask */
+#define M(X) (uint16_t) (1 << X)
+
+#define PHY_CTRL_RST            0x8000 /* PHY reset command */
+#define PHY_CTRL_ANEG_RST       0x0200 /* Autonegotiation reset command */
+
+/* PHY Advertisement control and remote capability registers (same bitfields) */
+#define PHY_ADVERTISE_10HALF    0x0020  /* Try for 10mbps half-duplex  */
+#define PHY_ADVERTISE_10FULL    0x0040  /* Try for 10mbps full-duplex  */
+#define PHY_ADVERTISE_100HALF   0x0080  /* Try for 100mbps half-duplex */
+#define PHY_ADVERTISE_100FULL   0x0100  /* Try for 100mbps full-duplex */
+
+#define TYPE_ETH_PHY "eth-phy"
+
+#define ETHPHY(obj) OBJECT_CHECK(EthPhy, (obj), TYPE_ETH_PHY)
+#define ETHPHY_CLASS(klass) OBJECT_CLASS_CHECK(EthPhyClass, (klass),\
+                                               TYPE_ETH_PHY)
+#define ETHPHY_GET_CLASS(obj) \
+     OBJECT_GET_CLASS(EthPhyClass, (obj), TYPE_ETH_PHY)
+
+static const uint16_t default_readonly_mask[32] = {
+    [PHY_CTRL] = PHY_CTRL_RST | PHY_CTRL_ANEG_RST,
+    [PHY_ID1] = 0xffff,
+    [PHY_ID2] = 0xffff,
+    [PHY_LP_ABILITY] = 0xffff,
+};
+
+typedef struct PhyPartInfo {
+    const char *partname;
+
+    uint16_t phy_id1;
+    uint16_t phy_id2;
+
+    bool autoneg;
+    bool gmii;
+} PhyPartInfo;
+
+#define PHYINFO(_part_name, _id1, _id2, _autoneg, _gmii) \
+    .partname = (_part_name),\
+    .phy_id1 = (_id1),\
+    .phy_id2 = (_id2),\
+    .autoneg = (_autoneg),\
+    .gmii = (_gmii),\
+
+static const PhyPartInfo devices[] = {
+    { PHYINFO("88e1116r", 0x0141, 0x0e50, 1, 1) },
+    { PHYINFO("dp83867",  0x2000, 0xa231, 1, 1) },
+    { PHYINFO("88e1118r", 0x0141, 0x0e10, 1, 1)},
+};
+
+
+typedef struct EthPhy {
+    MDIOSlave parent_object;
+
+    /* Only the basic registers, rest of them in Vendor */
+    uint16_t regs[NUM_PHY_REGS];
+    const uint16_t *regs_readonly_mask; /* 0=writable, 1=read-only */
+
+    bool link;
+
+    PhyPartInfo *part;
+} EthPhy;
+
+typedef struct EthPhyClass {
+    MDIOSlaveClass parent_class;
+
+    PhyPartInfo *part;
+} EthPhyClass;
+
+#endif
