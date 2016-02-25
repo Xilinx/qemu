65bd05a1bda800d6f22c98360fee359b40df4472
Alistair Francis <alistair.francis@xilinx.com>
2016-02-25 15:49:16 -0800

    fdt: Add an alias for the GPIO device
    Signed-off-by: Alistair Francis <alistair.francis@xilinx.com>

--- hw/core/fdt_generic_devices.c
+++ hw/core/fdt_generic_devices.c
@@ -217,6 +217,7 @@ static const TypeInfo fdt_qom_aliases [] = {
     {   .name = "xlnx.xuartps",             .parent = "cadence_uart"        },
     {   .name = "simple-bus",               .parent = "qemu:memory-region"  },
     {   .name = "xlnx,axi-dpdma-1.0",       .parent = "xlnx.dpdma"          },
+    {   .name = "xlnx,xps-gpio-1.00.a",     .parent = "xlnx.axi-gpio"       },
 };
 
 static void fdt_generic_register_types(void)
