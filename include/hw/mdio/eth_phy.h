de453ab4cf993f63de502c4efd8a08a347c3b164
Sai Pavan Boddu <sai.pavan.boddu@xilinx.com>
2016-09-15 07:56:00 -0700

    eth_phy: Add the 88e1116 PHY device to list of Ethernet PHYs
    Add the 88e1116 PHY to the list of supported Ethernet PHYs.
    This is the same as the 88e1116r and there is no fuctional difference
    to the emulation.
    
    Signed-off-by: Sai Pavan Boddu <saipava@xilinx.com>
    Reviewed-off-by: Alistair Francis <alistair.francis@xilinx.com>

--- include/hw/mdio/eth_phy.h
+++ include/hw/mdio/eth_phy.h
@@ -132,6 +132,7 @@ typedef struct PhyPartInfo {
 
 static const PhyPartInfo devices[] = {
     { PHYINFO("88e1116r", 0x0141, 0x0e50, 1, 1) },
+    { PHYINFO("88e1116",  0x0141, 0x0e50, 1, 1) },
     { PHYINFO("dp83867",  0x2000, 0xa231, 1, 1) },
     { PHYINFO("88e1118r", 0x0141, 0x0e10, 1, 1) },
     { PHYINFO("88e1510",  0x0141, 0x0dd0, 1, 1) },
