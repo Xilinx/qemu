f0ff16e9f2b31605995c15c27091bfcabcd80641
Sai Pavan Boddu <sai.pavan.boddu@xilinx.com>
2016-11-17 23:42:00 -0800

    fdt_generic_devices: Add a default phy model
    If compatible property is not defined, default it to "88e1116r".
    
    Signed-off-by: Sai Pavan Boddu <saipava@xilinx.com>
    Reviewed-by: Alistair Francis <alistair.francis@xilinx.com>

--- hw/core/fdt_generic_devices.c
+++ hw/core/fdt_generic_devices.c
@@ -211,6 +211,7 @@ static const TypeInfo fdt_qom_aliases [] = {
     {   .name = "arasan,sdhci-8.9a",        .parent = "xilinx.zynqmp-sdhci" },
     {   .name = "cdns.spi-r1p6",            .parent = "xlnx.ps7-spi"        },
     {   .name = "xlnx,eth-dma",             .parent = "xlnx.axi-dma"        },
+    {   .name = "device_type:ethernet-phy", .parent = "88e1116r"            },
 };
 
 static void fdt_generic_register_types(void)
