e3981132cc13911f7b96968fd2c0e2d20361ed09
Edgar E. Iglesias <edgar.iglesias@xilinx.com>
2017-03-26 21:22:16 +0200

    remote-port: Fix remote-port-qdev
    CONFIG_REMOTE_PORT is never set in C code, so we end up always
    using the code that exits due to lack of remote-port support.
    
    Command-line instantiation will fail when creating the remote-port
    device (i.e prior to trying to attach clients to it) if remote-port
    is not compiled in, so rp_device_add can be called either way.
    
    Move rp_device_attach into remote-port-qdev to avoid linking
    missmatches when remote-port is not enabled and always use
    rp_device_add as if remote-port is available.
    
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>
    Reviewed-by: Alistair Francis <alistair.francis@xilinx.com>

--- hw/core/remote-port-qdev.c
+++ hw/core/remote-port-qdev.c
@@ -18,10 +18,71 @@
 #include "qemu/config-file.h"
 #include "qemu/error-report.h"
 #include "qemu/help_option.h"
+#include "qemu/cutils.h"
 
 #include "hw/remote-port.h"
 
-#ifdef CONFIG_REMOTE_PORT
+/* RP helper function to attach a device to an adaptor.  */
+void rp_device_attach(Object *adaptor, Object *dev,
+                      int rp_nr, int dev_nr,
+                      Error **errp)
+{
+    Error *err = NULL;
+    uint32_t nr_devs;
+    char *name;
+    int i;
+
+    assert(adaptor);
+    assert(dev);
+
+    /* Verify that the adaptor is of Remote Port type.  */
+    if (!object_dynamic_cast(adaptor, TYPE_REMOTE_PORT)) {
+        error_setg(errp, "%s is not a Remote-Port adaptor!\n",
+                   object_get_canonical_path(adaptor));
+        return;
+    }
+
+    name = g_strdup_printf("rp-adaptor%d", rp_nr);
+    object_property_set_link(dev, adaptor, name, &err);
+    g_free(name);
+    if (err != NULL) {
+        error_propagate(errp, err);
+        return;
+    }
+
+    name = g_strdup_printf("rp-chan%d", rp_nr);
+    object_property_set_int(dev, dev_nr, name, &err);
+    g_free(name);
+    if (err != NULL
+        && !object_dynamic_cast(dev, TYPE_REMOTE_PORT_DEVICE)) {
+        /*
+         * RP devices that only receive requests may not need to
+         * know their channel/dev number. If not, treat this as
+         * an error.
+         */
+        error_propagate(errp, err);
+        return;
+    }
+    err = NULL;
+
+    nr_devs = object_property_get_int(dev, "nr-devs", &err);
+    if (err) {
+        nr_devs = 1;
+        err = NULL;
+    }
+
+    /* Multi-channel devs use consecutive numbering.  */
+    for (i = 0; i < nr_devs; i++) {
+        name = g_strdup_printf("remote-port-dev%d", dev_nr + i);
+        object_property_set_link(adaptor, dev, name, &err);
+        g_free(name);
+        if (err != NULL) {
+            error_propagate(errp, err);
+            return;
+        }
+    }
+}
+
 /* Scan for remote-port links to be setup.  */
 void rp_device_add(QemuOpts *opts, DeviceState *dev, Error **errp)
 {
@@ -82,25 +143,3 @@ void rp_device_add(QemuOpts *opts, DeviceState *dev, Error **errp)
         }
     }
 }
-#else
-void rp_device_add(QemuOpts *opts, DeviceState *dev, Error **errp)
-{
-    const char *path;
-    char *name;
-
-    /*
-     * Find the adaptor this remote-port device is connected to.
-     * At the moment, we only support one adaptor per device.
-     */
-    name = g_strdup_printf("rp-adaptor%d", 0);
-    path = qemu_opt_get(opts, name);
-    g_free(name);
-    if (!path) {
-        /* This is not a remote-port device.  */
-        return;
-    }
-
-    error_setg(errp, "Remote Port was not enabled at compile time.\n");
-    return;
-}
-#endif
