37a795e79d3f278ffdabacc87b63d2d4e32f0de2
Edgar E. Iglesias <edgar.iglesias@xilinx.com>
2018-07-25 18:16:46 +0700

    hw/core/reset: Refactor list handling
    Refactor list handling to make it easier to add multiple
    reset lists.
    
    No functional change.
    
    Reviewed-by: Sai Pavan Boddu <saipav@xilinx.com>
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- hw/core/reset.c
+++ hw/core/reset.c
@@ -38,26 +38,32 @@ typedef struct QEMUResetEntry {
 static QTAILQ_HEAD(reset_handlers, QEMUResetEntry) reset_handlers =
     QTAILQ_HEAD_INITIALIZER(reset_handlers);
 
+#define REGISTER_RESET_LIST(head, func, opaque)                 \
+    {                                                           \
+        QEMUResetEntry *re = g_malloc0(sizeof(QEMUResetEntry)); \
+        re->func = func;                                        \
+        re->opaque = opaque;                                    \
+        QTAILQ_INSERT_TAIL(head, re, entry);                    \
+    }
+
+#define UNREGISTER_RESET_LIST(head, func, opaque)               \
+    QEMUResetEntry *re;                                         \
+    QTAILQ_FOREACH(re, head, entry) {                           \
+        if (re->func == func && re->opaque == opaque) {         \
+            QTAILQ_REMOVE(head, re, entry);                     \
+            g_free(re);                                         \
+            return;                                             \
+        }                                                       \
+    }
+
 void qemu_register_reset(QEMUResetHandler *func, void *opaque)
 {
-    QEMUResetEntry *re = g_malloc0(sizeof(QEMUResetEntry));
-
-    re->func = func;
-    re->opaque = opaque;
-    QTAILQ_INSERT_TAIL(&reset_handlers, re, entry);
+    REGISTER_RESET_LIST(&reset_handlers, func, opaque);
 }
 
 void qemu_unregister_reset(QEMUResetHandler *func, void *opaque)
 {
-    QEMUResetEntry *re;
-
-    QTAILQ_FOREACH(re, &reset_handlers, entry) {
-        if (re->func == func && re->opaque == opaque) {
-            QTAILQ_REMOVE(&reset_handlers, re, entry);
-            g_free(re);
-            return;
-        }
-    }
+    UNREGISTER_RESET_LIST(&reset_handlers, func, opaque);
 }
 
 void qemu_devices_reset(void)
