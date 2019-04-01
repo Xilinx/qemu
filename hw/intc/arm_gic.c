63c753d9906b3d3a650af6e6baadb5b3d2a4b89f
Sai Pavan Boddu <sai.pavan.boddu@xilinx.com>
2019-04-01 20:30:30 +0530

    intc: arm_gic: Use num_cpu property for cpu id rollover
    Hard-coding cpu rollover does not work with versal. So use cpu_num property.
    
    Signed-off-by: Sai Pavan Boddu <sai.pavan.boddu@xilinx.com>
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- hw/intc/arm_gic.c
+++ hw/intc/arm_gic.c
@@ -93,7 +93,7 @@ static inline void gic_dump_lrs(GICState *s, const char *prefix)
 static inline int gic_get_current_cpu(GICState *s)
 {
     if (s->num_cpu > 1 && current_cpu) {
-        return current_cpu->cpu_index % 4;
+        return current_cpu->cpu_index % s->num_cpu;
     }
     return 0;
 }
