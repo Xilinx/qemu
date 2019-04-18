49372e282c5b1758d25ed72ac5e54290ff07fa1e
Edgar E. Iglesias <edgar.iglesias@xilinx.com>
2019-04-18 22:19:11 +0200

    target/arm: Clear WFI at reset
    After reset, we may be halted due to sleep halt signals but
    we are not in WFI. Clear WFI at reset.
    
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- target/arm/cpu.c
+++ target/arm/cpu.c
@@ -366,8 +366,8 @@ static void arm_cpu_reset(CPUState *s)
     }
 #endif
 
-    cpu->is_in_wfi = s->halted;
-    qemu_set_irq(cpu->wfi, s->halted);
+    cpu->is_in_wfi = false;
+    qemu_set_irq(cpu->wfi, cpu->is_in_wfi);
 
     hw_breakpoint_update_all(cpu);
     hw_watchpoint_update_all(cpu);
