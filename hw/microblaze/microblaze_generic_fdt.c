41d4ebc115dd883cb34984cb938e80b648ebe3eb
Edgar E. Iglesias <edgar.iglesias@xilinx.com>
2019-10-30 19:38:52 +0100

    hw/microblaze: Remove broken CPU FDT init
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- hw/microblaze/microblaze_generic_fdt.c
+++ hw/microblaze/microblaze_generic_fdt.c
@@ -53,149 +53,10 @@
 
 #include <libfdt.h>
 
-#define VAL(name) qemu_fdt_getprop_cell(fdt_g, node_path, name, 0, false, NULL)
-
 #define IS_PETALINUX_MACHINE \
     (!strcmp(MACHINE_GET_CLASS(machine)->name, MACHINE_NAME "-plnx"))
 
 #define QTEST_RUNNING (qtest_enabled() && qtest_driver())
-
-/* FIXME: delete */
-static void *fdt_g;
-
-static void
-microblaze_generic_fdt_reset(MicroBlazeCPU *cpu)
-{
-    CPUMBState *env = &cpu->env;
-
-    char node_path[DT_PATH_LENGTH];
-    qemu_devtree_get_node_by_name(fdt_g, node_path, "cpu");
-    int t;
-    int use_exc = 0;
-
-    if (fdt_path_offset(fdt_g, node_path) < 0) {
-        return;
-    }
-
-    env->pvr.regs[0] = 0;
-    env->pvr.regs[2] = PVR2_D_OPB_MASK \
-                        | PVR2_D_LMB_MASK \
-                        | PVR2_I_OPB_MASK \
-                        | PVR2_I_LMB_MASK \
-                        | 0;
-
-    /* Even if we don't have PVR's, we fill out everything
-       because QEMU will internally follow what the pvr regs
-       state about the HW.  */
-
-    if (VAL("xlnx,opcode-0x0-illegal")) {
-        env->pvr.regs[2] |= PVR2_OPCODE_0x0_ILL_MASK;
-    }
-
-    if (VAL("xlnx,unaligned-exceptions")) {
-        env->pvr.regs[2] |= PVR2_UNALIGNED_EXC_MASK;
-        use_exc = 1;
-    }
-
-    if (VAL("xlnx,ill-opcode-exception")) {
-        env->pvr.regs[2] |= PVR2_ILL_OPCODE_EXC_MASK;
-        use_exc = 1;
-    }
-
-    if (VAL("xlnx,div-zero-exception")) {
-        env->pvr.regs[2] |= PVR2_DIV_ZERO_EXC_MASK;
-        use_exc = 1;
-    }
-
-    env->pvr.regs[0] |= VAL("xlnx,pvr-user1") & 0xff;
-    env->pvr.regs[1] = VAL("xlnx,pvr-user2");
-
-    if (use_exc) {
-        env->pvr.regs[0] |= PVR0_USE_EXC_MASK ;
-    }
-
-    t = VAL("xlnx,mmu-zones");
-    env->pvr.regs[11] |= t << 17;
-    env->mmu.c_mmu_zones = t;
-
-    t = VAL("xlnx,mmu-tlb-access");
-    env->mmu.c_mmu_tlb_access = t;
-    env->pvr.regs[11] |= t << 22;
-
-    {
-        char *str;
-        const struct {
-            const char *name;
-            unsigned int arch;
-        } arch_lookup[] = {
-            {"virtex2", 0x4},
-            {"virtex2pro", 0x5},
-            {"spartan3", 0x6},
-            {"virtex4", 0x7},
-            {"virtex5", 0x8},
-            {"spartan3e", 0x9},
-            {"spartan3a", 0xa},
-            {"spartan3an", 0xb},
-            {"spartan3adsp", 0xc},
-            {"spartan6", 0xd},
-            {"virtex6", 0xe},
-            {"virtex7", 0xf},
-            {"kintex7", 0x10},
-            {"artix7", 0x11},
-            {"zynq7000", 0x12},
-            {"spartan2", 0xf0},
-            {NULL, 0},
-        };
-        unsigned int i = 0;
-
-        str = qemu_fdt_getprop(fdt_g, node_path, "xlnx,family", NULL, false, NULL);
-        while (arch_lookup[i].name && str) {
-            if (strcmp(arch_lookup[i].name, str) == 0) {
-                break;
-            }
-            i++;
-        }
-
-        env->pvr.regs[10] &= ~0xff000000;
-        if (!str || !arch_lookup[i].arch) {
-            env->pvr.regs[10] |= 0x0c000000; /* spartan 3a dsp family.  */
-        } else {
-            env->pvr.regs[10] |= arch_lookup[i].arch << 24;
-        }
-        g_free(str);
-    }
-
-    {
-        env->pvr.regs[4] = PVR4_USE_ICACHE_MASK
-                           | (21 << 26) /* Tag size.  */
-                           | (4 << 21)
-                           | (11 << 16);
-        env->pvr.regs[6] = VAL("d-cache-baseaddr");
-        env->pvr.regs[7] = VAL("d-cache-highaddr");
-        env->pvr.regs[5] = PVR5_USE_DCACHE_MASK
-                           | (21 << 26) /* Tag size.  */
-                           | (4 << 21)
-                           | (11 << 16);
-
-        if (VAL("xlnx,dcache-use-writeback")) {
-            env->pvr.regs[5] |= PVR5_DCACHE_WRITEBACK_MASK;
-        }
-
-        env->pvr.regs[8] = VAL("i-cache-baseaddr");
-        env->pvr.regs[9] = VAL("i-cache-highaddr");
-    }
-    if (VAL("qemu,halt")) {
-        cpu_interrupt(env_cpu(env), CPU_INTERRUPT_HALT);
-    }
-}
-
-static void secondary_cpu_reset(void *opaque)
-{
-    MicroBlazeCPU *cpu = MICROBLAZE_CPU(opaque);
-
-    /* Configure secondary cores.  */
-    microblaze_generic_fdt_reset(cpu);
-}
 
 #define LMB_BRAM_SIZE  (128 * 1024)
 
@@ -210,7 +71,6 @@ int endian;
 static void
 microblaze_generic_fdt_init(MachineState *machine)
 {
-    CPUState *cpu;
     ram_addr_t ram_kernel_base = 0, ram_kernel_size = 0;
     void *fdt = NULL;
     const char *dtb_arg, *hw_dtb_arg;
@@ -362,19 +222,11 @@ microblaze_generic_fdt_init(MachineState *machine)
 
     fdt_init_destroy_fdti(fdti);
 
-    fdt_g = fdt;
     kernel_filename = qemu_opt_get(machine_opts, "kernel");
     if (kernel_filename) {
         microblaze_load_kernel(MICROBLAZE_CPU(first_cpu), ram_kernel_base,
                                ram_kernel_size, machine->initrd_filename, NULL,
-                               microblaze_generic_fdt_reset, fdt, fdt_size);
-    }
-
-    /* Register FDT to prop mapper for secondary cores.  */
-    cpu = CPU_NEXT(first_cpu);
-    while (cpu) {
-        qemu_register_reset(secondary_cpu_reset, cpu);
-        cpu = CPU_NEXT(cpu);
+                               NULL, fdt, fdt_size);
     }
 
     return;
