b3e6f17c0afa6b5a2d4f2ee3c368c430bd809c5e
Jaewook Bae <jaewook.bae@xilinx.com>
2020-02-06 08:55:59 -0800

    zynqmp: lpd-slcr-secure: Convert to new register api model
    Replace dep-registers with new registers.
    No functional changes.
    
    Signed-off-by: Jaewook Bae <jaewookb@xilinx.com>
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- hw/misc/xlnx-lpd-slcr-secure.c
+++ hw/misc/xlnx-lpd-slcr-secure.c
@@ -26,10 +26,11 @@
 #include "hw/sysbus.h"
 #include "migration/vmstate.h"
 #include "hw/qdev-properties.h"
-#include "hw/register-dep.h"
+#include "hw/register.h"
 #include "qemu/bitops.h"
 #include "qapi/qmp/qerror.h"
 #include "qemu/log.h"
+#include "hw/irq.h"
 
 #ifndef XILINX_LPD_SLCR_SECURE_ERR_DEBUG
 #define XILINX_LPD_SLCR_SECURE_ERR_DEBUG 0
@@ -40,27 +41,27 @@
 #define XILINX_LPD_SLCR_SECURE(obj) \
      OBJECT_CHECK(LPDSLCRSecure, (obj), TYPE_XILINX_LPD_SLCR_SECURE)
 
-DEP_REG32(CTRL, 0x4)
-    DEP_FIELD(CTRL, SLVERR_ENABLE, 1, 0)
-DEP_REG32(ISR, 0x8)
-    DEP_FIELD(ISR, ADDR_DECODE_ERR, 1, 0)
-DEP_REG32(IMR, 0xc)
-    DEP_FIELD(IMR, ADDR_DECODE_ERR, 1, 0)
-DEP_REG32(IER, 0x10)
-    DEP_FIELD(IER, ADDR_DECODE_ERR, 1, 0)
-DEP_REG32(IDR, 0x14)
-    DEP_FIELD(IDR, ADDR_DECODE_ERR, 1, 0)
-DEP_REG32(ITR, 0x18)
-    DEP_FIELD(ITR, ADDR_DECODE_ERR, 1, 0)
-DEP_REG32(SLCR_RPU, 0x20)
-    DEP_FIELD(SLCR_RPU, TZ_R5_0, 1, 0)
-    DEP_FIELD(SLCR_RPU, TZ_R5_1, 1, 1)
-DEP_REG32(SLCR_ADMA, 0x24)
-    DEP_FIELD(SLCR_RPU, TZ, 1, 0)
-DEP_REG32(SAFETY_CHK, 0x30)
-DEP_REG32(SLCR_USB, 0x34)
-    DEP_FIELD(SLCR_USB, TZ_USB3_0, 1, 0)
-    DEP_FIELD(SLCR_USB, TZ_USB3_1, 1, 1)
+REG32(CTRL, 0x4)
+    FIELD(CTRL, SLVERR_ENABLE, 0, 1)
+REG32(ISR, 0x8)
+    FIELD(ISR, ADDR_DECODE_ERR, 0, 1)
+REG32(IMR, 0xc)
+    FIELD(IMR, ADDR_DECODE_ERR, 0, 1)
+REG32(IER, 0x10)
+    FIELD(IER, ADDR_DECODE_ERR, 0, 1)
+REG32(IDR, 0x14)
+    FIELD(IDR, ADDR_DECODE_ERR, 0, 1)
+REG32(ITR, 0x18)
+    FIELD(ITR, ADDR_DECODE_ERR, 0, 1)
+REG32(SLCR_RPU, 0x20)
+    FIELD(SLCR_RPU, TZ_R5_1, 1, 1)
+    FIELD(SLCR_RPU, TZ_R5_0, 0, 1)
+REG32(SLCR_ADMA, 0x24)
+    FIELD(SLCR_ADMA, TZ, 0, 8)
+REG32(SAFETY_CHK, 0x30)
+REG32(SLCR_USB, 0x34)
+    FIELD(SLCR_USB, TZ_USB3_1, 1, 1)
+    FIELD(SLCR_USB, TZ_USB3_0, 0, 1)
 
 #define R_MAX (R_SLCR_USB + 1)
 
@@ -70,7 +71,7 @@ typedef struct LPDSLCRSecure {
     qemu_irq irq_isr;
 
     uint32_t regs[R_MAX];
-    DepRegisterInfo regs_info[R_MAX];
+    RegisterInfo regs_info[R_MAX];
 } LPDSLCRSecure;
 
 static void isr_update_irq(LPDSLCRSecure *s)
@@ -79,13 +80,13 @@ static void isr_update_irq(LPDSLCRSecure *s)
     qemu_set_irq(s->irq_isr, pending);
 }
 
-static void isr_postw(DepRegisterInfo *reg, uint64_t val64)
+static void isr_postw(RegisterInfo *reg, uint64_t val64)
 {
     LPDSLCRSecure *s = XILINX_LPD_SLCR_SECURE(reg->opaque);
     isr_update_irq(s);
 }
 
-static uint64_t ier_prew(DepRegisterInfo *reg, uint64_t val64)
+static uint64_t ier_prew(RegisterInfo *reg, uint64_t val64)
 {
     LPDSLCRSecure *s = XILINX_LPD_SLCR_SECURE(reg->opaque);
     uint32_t val = val64;
@@ -95,7 +96,7 @@ static uint64_t ier_prew(DepRegisterInfo *reg, uint64_t val64)
     return 0;
 }
 
-static uint64_t idr_prew(DepRegisterInfo *reg, uint64_t val64)
+static uint64_t idr_prew(RegisterInfo *reg, uint64_t val64)
 {
     LPDSLCRSecure *s = XILINX_LPD_SLCR_SECURE(reg->opaque);
     uint32_t val = val64;
@@ -105,7 +106,7 @@ static uint64_t idr_prew(DepRegisterInfo *reg, uint64_t val64)
     return 0;
 }
 
-static uint64_t itr_prew(DepRegisterInfo *reg, uint64_t val64)
+static uint64_t itr_prew(RegisterInfo *reg, uint64_t val64)
 {
     LPDSLCRSecure *s = XILINX_LPD_SLCR_SECURE(reg->opaque);
     uint32_t val = val64;
@@ -115,24 +116,24 @@ static uint64_t itr_prew(DepRegisterInfo *reg, uint64_t val64)
     return 0;
 }
 
-static DepRegisterAccessInfo lpd_slcr_secure_regs_info[] = {
-    { .name = "CTRL",  .decode.addr = A_CTRL,
-    },{ .name = "ISR",  .decode.addr = A_ISR,
+static RegisterAccessInfo lpd_slcr_secure_regs_info[] = {
+    { .name = "CTRL",  .addr = A_CTRL,
+    },{ .name = "ISR",  .addr = A_ISR,
         .w1c = 0x1,
         .post_write = isr_postw,
-    },{ .name = "IMR",  .decode.addr = A_IMR,
+    },{ .name = "IMR",  .addr = A_IMR,
         .reset = 0x1,
         .ro = 0x1,
-    },{ .name = "IER",  .decode.addr = A_IER,
+    },{ .name = "IER",  .addr = A_IER,
         .pre_write = ier_prew,
-    },{ .name = "IDR",  .decode.addr = A_IDR,
+    },{ .name = "IDR",  .addr = A_IDR,
         .pre_write = idr_prew,
-    },{ .name = "ITR",  .decode.addr = A_ITR,
+    },{ .name = "ITR",  .addr = A_ITR,
         .pre_write = itr_prew,
-    },{ .name = "SLCR_RPU",   .decode.addr = A_SLCR_RPU,
-    },{ .name = "SLCR_ADMA",  .decode.addr = A_SLCR_ADMA,
-    },{ .name = "SAFETY_CHK", .decode.addr = A_SAFETY_CHK,
-    },{ .name = "SLCR_USB",   .decode.addr = A_SLCR_USB,
+    },{ .name = "SLCR_RPU",   .addr = A_SLCR_RPU,
+    },{ .name = "SLCR_ADMA",  .addr = A_SLCR_ADMA,
+    },{ .name = "SAFETY_CHK", .addr = A_SAFETY_CHK,
+    },{ .name = "SLCR_USB",   .addr = A_SLCR_USB,
     }
 };
 
@@ -142,44 +143,15 @@ static void lpd_slcr_secure_reset(DeviceState *dev)
     unsigned int i;
 
     for (i = 0; i < ARRAY_SIZE(s->regs_info); ++i) {
-        dep_register_reset(&s->regs_info[i]);
+        register_reset(&s->regs_info[i]);
     }
 
     isr_update_irq(s);
 }
 
-static uint64_t lpd_slcr_secure_read(void *opaque, hwaddr addr, unsigned size)
-{
-    LPDSLCRSecure *s = XILINX_LPD_SLCR_SECURE(opaque);
-    DepRegisterInfo *r = &s->regs_info[addr / 4];
-
-    if (!r->data) {
-        qemu_log("%s: Decode error: read from %" HWADDR_PRIx "\n",
-                 object_get_canonical_path(OBJECT(s)),
-                 addr);
-        return 0;
-    }
-    return dep_register_read(r);
-}
-
-static void lpd_slcr_secure_write(void *opaque, hwaddr addr, uint64_t value,
-                                  unsigned size)
-{
-    LPDSLCRSecure *s = XILINX_LPD_SLCR_SECURE(opaque);
-    DepRegisterInfo *r = &s->regs_info[addr / 4];
-
-    if (!r->data) {
-        qemu_log("%s: Decode error: write to %" HWADDR_PRIx "=%" PRIx64 "\n",
-                 object_get_canonical_path(OBJECT(s)),
-                 addr, value);
-        return;
-    }
-    dep_register_write(r, value, ~0);
-}
-
 static const MemoryRegionOps lpd_slcr_secure_ops = {
-    .read = lpd_slcr_secure_read,
-    .write = lpd_slcr_secure_write,
+    .read = register_read_memory,
+    .write = register_write_memory,
     .endianness = DEVICE_LITTLE_ENDIAN,
     .valid = {
         .min_access_size = 4,
@@ -189,33 +161,26 @@ static const MemoryRegionOps lpd_slcr_secure_ops = {
 
 static void lpd_slcr_secure_realize(DeviceState *dev, Error **errp)
 {
-    LPDSLCRSecure *s = XILINX_LPD_SLCR_SECURE(dev);
-    const char *prefix = object_get_canonical_path(OBJECT(dev));
-    unsigned int i;
-
-    for (i = 0; i < ARRAY_SIZE(lpd_slcr_secure_regs_info); ++i) {
-        DepRegisterInfo *r =
-                &s->regs_info[lpd_slcr_secure_regs_info[i].decode.addr / 4];
-
-        *r = (DepRegisterInfo) {
-            .data = (uint8_t *)&s->regs[
-                    lpd_slcr_secure_regs_info[i].decode.addr / 4],
-            .data_size = sizeof(uint32_t),
-            .access = &lpd_slcr_secure_regs_info[i],
-            .debug = XILINX_LPD_SLCR_SECURE_ERR_DEBUG,
-            .prefix = prefix,
-            .opaque = s,
-        };
-    }
+    /* Delete this if not necessary */
 }
 
 static void lpd_slcr_secure_init(Object *obj)
 {
     LPDSLCRSecure *s = XILINX_LPD_SLCR_SECURE(obj);
     SysBusDevice *sbd = SYS_BUS_DEVICE(obj);
+    RegisterInfoArray *reg_array;
 
-    memory_region_init_io(&s->iomem, obj, &lpd_slcr_secure_ops, s,
-                          TYPE_XILINX_LPD_SLCR_SECURE, R_MAX * 4);
+    memory_region_init(&s->iomem, obj, TYPE_XILINX_LPD_SLCR_SECURE, R_MAX * 4);
+
+    reg_array =
+        register_init_block32(DEVICE(obj), lpd_slcr_secure_regs_info,
+                              ARRAY_SIZE(lpd_slcr_secure_regs_info),
+                              s->regs_info, s->regs,
+                              &lpd_slcr_secure_ops,
+                              XILINX_LPD_SLCR_SECURE_ERR_DEBUG,
+                              R_MAX * 4);
+    memory_region_add_subregion(&s->iomem, 0x0, &reg_array->mem);
+
     sysbus_init_mmio(sbd, &s->iomem);
     sysbus_init_irq(sbd, &s->irq_isr);
 }
