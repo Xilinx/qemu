2d17a3988fab7432c6ca79d3fdd98abdb66b07ee
Jaewook Bae <jaewook.bae@xilinx.com>
2020-02-07 10:57:58 -0800

    hw/dma: Convert devcfg to new register api model
    Replace dep-registers with new registers.
    No functional changes.
    CAUTIOUS: Since this file was not compiled during make, only changes were
              made but was not tested in compile and validation.
    
    Signed-off-by: Jaewook Bae <jaewookb@xilinx.com>
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- hw/dma/xilinx_devcfg.c
+++ hw/dma/xilinx_devcfg.c
@@ -2,7 +2,7 @@
  * QEMU model of the Xilinx Devcfg Interface
  *
  * (C) 2011 PetaLogix Pty Ltd
- * (C) 2014 Xilinx Inc.
+ * (C) 2014-2020 Xilinx Inc.
  * Written by Peter Crosthwaite <peter.crosthwaite@xilinx.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -29,7 +29,7 @@
 #include "sysemu/dma.h"
 #include "hw/sysbus.h"
 #include "qemu/bitops.h"
-#include "hw/register-dep.h"
+#include "hw/register.h"
 #include "qapi/error.h"
 #include "qemu/log.h"
 #include "qemu/bitops.h"
@@ -55,26 +55,23 @@
     } \
 } while (0);
 
-#define R_CTRL            (0x00/4)
-    #define FORCE_RST            (1 << 31) /* Not supported, writes ignored */
-    #define PCAP_PR              (1 << 27) /* Forced to 0 on bad unlock */
-    #define PCAP_MODE            (1 << 26)
-    #define MULTIBOOT_EN         (1 << 24)
-    #define USER_MODE            (1 << 15)
-    #define PCFG_AES_FUSE        (1 << 12) /* locked by AES_FUSE_LOCK */
-    #define PCFG_AES_EN_SHIFT    9 /* locked by AES_EN_LOCK */
-    #define PCFG_AES_EN_LEN      3 /* locked by AES_EN_LOCK */
-    #define PCFG_AES_EN_MASK     (((1 << PCFG_AES_EN_LEN) - 1) \
-                                    << PCFG_AES_EN_SHIFT)
-    #define SEU_EN               (1 << 8) /* locked by SEU_LOCK */
-    #define SEC_EN               (1 << 7) /* locked by SEC_LOCK */
-    #define SPNIDEN              (1 << 6) /* locked by DBG_LOCK */
-    #define SPIDEN               (1 << 5) /* locked by DBG_LOCK */
-    #define NIDEN                (1 << 4) /* locked by DBG_LOCK */
-    #define DBGEN                (1 << 3) /* locked by DBG_LOCK */
-    #define DAP_EN               (7 << 0) /* locked by DBG_LOCK */
+REG32(CTRL, 0x00)
+    FIELD(CTRL,     FORCE_RST,          31,  1) /* Not supported, wr ignored */
+    FIELD(CTRL,     PCAP_PR,            27,  1) /* Forced to 0 on bad unlock */
+    FIELD(CTRL,     PCAP_MODE,          26,  1)
+    FIELD(CTRL,     MULTIBOOT_EN,       24,  1)
+    FIELD(CTRL,     USER_MODE,          15,  1)
+    FIELD(CTRL,     PCFG_AES_FUSE,      12,  1)
+    FIELD(CTRL,     PCFG_AES_EN,         9,  3)
+    FIELD(CTRL,     SEU_EN,              8,  1)
+    FIELD(CTRL,     SEC_EN,              7,  1)
+    FIELD(CTRL,     SPNIDEN,             6,  1)
+    FIELD(CTRL,     SPIDEN,              5,  1)
+    FIELD(CTRL,     NIDEN,               4,  1)
+    FIELD(CTRL,     DBGEN,               3,  1)
+    FIELD(CTRL,     DAP_EN,              0,  3)
 
-#define R_LOCK          (0x04/4)
+REG32(LOCK, 0x04)
     #define AES_FUSE_LOCK        4
     #define AES_EN_LOCK          3
     #define SEU_LOCK             2
@@ -90,63 +87,58 @@ static const uint32_t lock_ctrl_map[] = {
     [DBG_LOCK] =  SPNIDEN | SPIDEN | NIDEN | DBGEN | DAP_EN,
 };
 
-#define R_CFG           (0x08/4)
-    #define RFIFO_TH_SHIFT       10
-    #define RFIFO_TH_LEN         2
-    #define WFIFO_TH_SHIFT       8
-    #define WFIFO_TH_LEN         2
-    #define DISABLE_SRC_INC      (1 << 5)
-    #define DISABLE_DST_INC      (1 << 4)
+REG32(CFG, 0x08)
+    FIELD(CFG,      RFIFO_TH,           10,  2)
+    FIELD(CFG,      WFIFO_TH,            8,  2)
+    FIELD(CFG,      RCLK_EDGE,           7,  1)
+    FIELD(CFG,      WCLK_EDGE,           6,  1)
+    FIELD(CFG,      DISABLE_SRC_INC,     5,  1)
+    FIELD(CFG,      DISABLE_DST_INC,     4,  1)
 #define R_CFG_RO 0xFFFFF800
 #define R_CFG_RESET 0x50B
 
-#define R_INT_STS       (0x0C/4)
-    #define PSS_GTS_USR_B_INT    (1 << 31)
-    #define PSS_FST_CFG_B_INT    (1 << 30)
-    #define PSS_CFG_RESET_B_INT  (1 << 27)
-    #define RX_FIFO_OV_INT       (1 << 18)
-    #define WR_FIFO_LVL_INT      (1 << 17)
-    #define RD_FIFO_LVL_INT      (1 << 16)
-    #define DMA_CMD_ERR_INT      (1 << 15)
-    #define DMA_Q_OV_INT         (1 << 14)
-    #define DMA_DONE_INT         (1 << 13)
-    #define DMA_P_DONE_INT       (1 << 12)
-    #define P2D_LEN_ERR_INT      (1 << 11)
-    #define PCFG_DONE_INT        (1 << 2)
-    #define R_INT_STS_RSVD       ((0x7 << 24) | (0x1 << 19) | (0xF < 7))
+REG32(INT_STS, 0x0C)
+    FIELD(INT_STS,  PSS_GTS_USR_B,      31,  1)
+    FIELD(INT_STS,  PSS_FST_CFG_B,      30,  1)
+    FIELD(INT_STS,  PSS_CFG_RESET_B,    27,  1)
+    FIELD(INT_STS,  RX_FIFO_OV,         18,  1)
+    FIELD(INT_STS,  WR_FIFO_LVL,        17,  1)
+    FIELD(INT_STS,  RD_FIFO_LVL,        16,  1)
+    FIELD(INT_STS,  DMA_CMD_ERR,        15,  1)
+    FIELD(INT_STS,  DMA_Q_OV,           14,  1)
+    FIELD(INT_STS,  DMA_DONE,           13,  1)
+    FIELD(INT_STS,  DMA_P_DONE,         12,  1)
+    FIELD(INT_STS,  P2D_LEN_ERR,        11,  1)
+    FIELD(INT_STS,  PCFG_DONE,           2,  1)
+#define R_INT_STS_RSVD       ((0x7 << 24) | (0x1 << 19) | (0xF < 7))
 
-#define R_INT_MASK      (0x10/4)
+REG32(INT_MASK, 0x10)
 
-#define R_STATUS        (0x14/4)
-    #define DMA_CMD_Q_F         (1 << 31)
-    #define DMA_CMD_Q_E         (1 << 30)
-    #define DMA_DONE_CNT_SHIFT  28
-    #define DMA_DONE_CNT_LEN    2
-    #define RX_FIFO_LVL_SHIFT   20
-    #define RX_FIFO_LVL_LEN     5
-    #define TX_FIFO_LVL_SHIFT   12
-    #define TX_FIFO_LVL_LEN     7
-    #define TX_FIFO_LVL         (0x7f << 12)
-    #define PSS_GTS_USR_B       (1 << 11)
-    #define PSS_FST_CFG_B       (1 << 10)
-    #define PSS_CFG_RESET_B     (1 << 5)
+REG32(STATUS, 0x14)
+    FIELD(STATUS,   DMA_CMD_Q_F,        31,  1)
+    FIELD(STATUS,   DMA_CMD_Q_E,        30,  1)
+    FIELD(STATUS,   DMA_DONE_CNT,       28,  2)
+    FIELD(STATUS,   RX_FIFO_LVL,        20,  5)
+    FIELD(STATUS,   TX_FIFO_LVL,        12,  7)
+    FIELD(STATUS,   PSS_GTS_USR_B,      11,  1)
+    FIELD(STATUS,   PSS_FST_CFG_B,      10,  1)
+    FIELD(STATUS,   PSS_CFG_RESET_B,     5,  1)
 
-#define R_DMA_SRC_ADDR  (0x18/4)
-#define R_DMA_DST_ADDR  (0x1C/4)
-#define R_DMA_SRC_LEN   (0x20/4)
-#define R_DMA_DST_LEN   (0x24/4)
-#define R_ROM_SHADOW    (0x28/4)
-#define R_SW_ID         (0x30/4)
-#define R_UNLOCK        (0x34/4)
+REG32(DMA_SRC_ADDR, 0x18)
+REG32(DMA_DST_ADDR, 0x1C)
+REG32(DMA_SRC_LEN, 0x20)
+REG32(DMA_DST_LEN, 0x24)
+REG32(ROM_SHADOW, 0x28)
+REG32(SW_ID, 0x30)
+REG32(UNLOCK, 0x34)
 
 #define R_UNLOCK_MAGIC 0x757BDF0D
 
-#define R_MCTRL         (0x80/4)
-    #define PS_VERSION_SHIFT    28
-    #define PS_VERSION_MASK     (0xf << PS_VERSION_SHIFT)
-    #define PCFG_POR_B          (1 << 8)
-    #define INT_PCAP_LPBK       (1 << 4)
-    #define MCTRL_QEMU          (1 << 3)
+REG32(MCTRL, 0x80)
+    FIELD(MCTRL,    PS_VERSION,         28,  4)
+    FIELD(MCTRL,    PCFG_POR_B,          8,  1)
+    FIELD(MCTRL,    INT_PCAP_LPBK,       4,  1)
+    FIELD(MCTRL,    QEMU,                3,  1)
 
 #define R_MAX (0x118/4+1)
 
@@ -188,7 +180,7 @@ typedef struct XilinxDevcfg {
     uint8_t dma_command_fifo_num;
 
     uint32_t regs[R_MAX];
-    DepRegisterInfo regs_info[R_MAX];
+    RegisterInfo regs_info[R_MAX];
 } XilinxDevcfg;
 
 static const VMStateDescription vmstate_xilinx_devcfg = {
@@ -218,7 +210,7 @@ static void xilinx_devcfg_reset(DeviceState *dev)
     int i;
 
     for (i = 0; i < R_MAX; ++i) {
-        dep_register_reset(&s->regs_info[i]);
+        register_reset(&s->regs_info[i]);
     }
 }
 
@@ -257,14 +249,14 @@ static void xilinx_devcfg_dma_go(XilinxDevcfg *s)
     }
 }
 
-static void r_ixr_post_write(DepRegisterInfo *reg, uint64_t val)
+static void r_ixr_post_write(RegisterInfo *reg, uint64_t val)
 {
     XilinxDevcfg *s = XILINX_DEVCFG(reg->opaque);
 
     xilinx_devcfg_update_ixr(s);
 }
 
-static uint64_t r_ctrl_pre_write(DepRegisterInfo *reg, uint64_t val)
+static uint64_t r_ctrl_pre_write(RegisterInfo *reg, uint64_t val)
 {
     XilinxDevcfg *s = XILINX_DEVCFG(reg->opaque);
     int i;
@@ -278,7 +270,7 @@ static uint64_t r_ctrl_pre_write(DepRegisterInfo *reg, uint64_t val)
     return val;
 }
 
-static void r_ctrl_post_write(DepRegisterInfo *reg, uint64_t val)
+static void r_ctrl_post_write(RegisterInfo *reg, uint64_t val)
 {
     uint32_t aes_en = extract32(val, PCFG_AES_EN_SHIFT, PCFG_AES_EN_LEN);
 
@@ -289,7 +281,7 @@ static void r_ctrl_post_write(DepRegisterInfo *reg, uint64_t val)
     }
 }
 
-static void r_unlock_post_write(DepRegisterInfo *reg, uint64_t val)
+static void r_unlock_post_write(RegisterInfo *reg, uint64_t val)
 {
     XilinxDevcfg *s = XILINX_DEVCFG(reg->opaque);
 
@@ -302,7 +294,7 @@ static void r_unlock_post_write(DepRegisterInfo *reg, uint64_t val)
     }
 }
 
-static uint64_t r_lock_pre_write(DepRegisterInfo *reg, uint64_t val)
+static uint64_t r_lock_pre_write(RegisterInfo *reg, uint64_t val)
 {
     XilinxDevcfg *s = XILINX_DEVCFG(reg->opaque);
 
@@ -310,7 +302,7 @@ static uint64_t r_lock_pre_write(DepRegisterInfo *reg, uint64_t val)
     return s->regs[R_LOCK] | val;
 }
 
-static void r_dma_dst_len_post_write(DepRegisterInfo *reg, uint64_t val)
+static void r_dma_dst_len_post_write(RegisterInfo *reg, uint64_t val)
 {
     XilinxDevcfg *s = XILINX_DEVCFG(reg->opaque);
 
@@ -326,12 +318,12 @@ static void r_dma_dst_len_post_write(DepRegisterInfo *reg, uint64_t val)
     xilinx_devcfg_dma_go(s);
 }
 
-static const DepRegisterAccessInfo xilinx_devcfg_regs_info[] = {
-    {   .name = "CTRL",                 .decode.addr = R_CTRL * 4,
+static const RegisterAccessInfo xilinx_devcfg_regs_info[] = {
+    {   .name = "CTRL",                 .addr = R_CTRL * 4,
         .reset = PCAP_PR | PCAP_MODE | 0x3 << 13,
         .ro = 0x107f6000,
         .rsvd = 0x1 << 15 | 0x3 << 13,
-        .ui1 = (DepRegisterAccessError[]) {
+        .ui1 = (RegisterAccessError[]) {
             { .mask = FORCE_RST, .reason = "PS reset not implemented" },
             { .mask = PCAP_MODE, .reason = "FPGA Fabric doesnt exist" },
             { .mask = PCFG_AES_EN_MASK, .reason = "AES not implmented" },
@@ -340,46 +332,46 @@ static const DepRegisterAccessInfo xilinx_devcfg_regs_info[] = {
         .pre_write = r_ctrl_pre_write,
         .post_write = r_ctrl_post_write,
     },
-    {   .name = "LOCK",                 .decode.addr = R_LOCK * 4,
+    {   .name = "LOCK",                 .addr = R_LOCK * 4,
         .ro = ~ONES(5),
         .pre_write = r_lock_pre_write,
     },
-    {   .name = "CFG",                  .decode.addr = R_CFG * 4,
+    {   .name = "CFG",                  .addr = R_CFG * 4,
         .reset = 1 << RFIFO_TH_SHIFT | 1 << WFIFO_TH_SHIFT | 0x8,
         .rsvd = 0xf,
         .ro = 0x00f | ~ONES(12),
     },
-    {   .name = "INT_STS",              .decode.addr = R_INT_STS * 4,
+    {   .name = "INT_STS",              .addr = R_INT_STS * 4,
         .w1c = ~R_INT_STS_RSVD,
         .reset = PSS_GTS_USR_B_INT | PSS_CFG_RESET_B_INT | WR_FIFO_LVL_INT,
         .ro = R_INT_STS_RSVD,
         .post_write = r_ixr_post_write,
     },
-    {    .name = "INT_MASK",            .decode.addr = R_INT_MASK * 4,
+    {    .name = "INT_MASK",            .addr = R_INT_MASK * 4,
         .reset = ~0,
         .ro = R_INT_STS_RSVD,
         .post_write = r_ixr_post_write,
     },
-    {   .name = "STATUS",               .decode.addr = R_STATUS * 4,
+    {   .name = "STATUS",               .addr = R_STATUS * 4,
         .reset = DMA_CMD_Q_E | PSS_GTS_USR_B | PSS_CFG_RESET_B,
         .ro = ~0,
     },
-    {   .name = "DMA_SRC_ADDR",         .decode.addr = R_DMA_SRC_ADDR * 4, },
-    {   .name = "DMA_DST_ADDR",         .decode.addr = R_DMA_DST_ADDR * 4, },
-    {   .name = "DMA_SRC_LEN",          .decode.addr = R_DMA_SRC_LEN * 4,
+    {   .name = "DMA_SRC_ADDR",         .addr = R_DMA_SRC_ADDR * 4, },
+    {   .name = "DMA_DST_ADDR",         .addr = R_DMA_DST_ADDR * 4, },
+    {   .name = "DMA_SRC_LEN",          .addr = R_DMA_SRC_LEN * 4,
         .ro = ~ONES(27) },
-    {   .name = "DMA_DST_LEN",          .decode.addr = R_DMA_DST_LEN * 4,
+    {   .name = "DMA_DST_LEN",          .addr = R_DMA_DST_LEN * 4,
         .ro = ~ONES(27),
         .post_write = r_dma_dst_len_post_write,
     },
-    {   .name = "ROM_SHADOW",           .decode.addr = R_ROM_SHADOW * 4,
+    {   .name = "ROM_SHADOW",           .addr = R_ROM_SHADOW * 4,
         .rsvd = ~0ull,
     },
-    {   .name = "SW_ID",                .decode.addr = R_SW_ID * 4, },
-    {   .name = "UNLOCK",               .decode.addr = R_UNLOCK * 4,
+    {   .name = "SW_ID",                .addr = R_SW_ID * 4, },
+    {   .name = "UNLOCK",               .addr = R_UNLOCK * 4,
         .post_write = r_unlock_post_write,
     },
-    {   .name = "MCTRL",                .decode.addr = R_MCTRL * 4,
+    {   .name = "MCTRL",                .addr = R_MCTRL * 4,
        /* Silicon 3.0 for version field, and the mysterious reserved bit 23 */
        .reset = 0x2 << PS_VERSION_SHIFT | 1 << 23 | MCTRL_QEMU,
        /* some reserved bits are rw while others are ro */
@@ -389,8 +381,8 @@ static const DepRegisterAccessInfo xilinx_devcfg_regs_info[] = {
 };
 
 static const MemoryRegionOps devcfg_reg_ops = {
-    .read = dep_register_read_memory_le,
-    .write = dep_register_write_memory_le,
+    .read = register_read_memory,
+    .write = register_write_memory,
     .endianness = DEVICE_LITTLE_ENDIAN,
     .valid = {
         .min_access_size = 4,
@@ -401,31 +393,12 @@ static const MemoryRegionOps devcfg_reg_ops = {
 static void xilinx_devcfg_realize(DeviceState *dev, Error **errp)
 {
     XilinxDevcfg *s = XILINX_DEVCFG(dev);
-    const char *prefix = object_get_canonical_path(OBJECT(dev));
-    int i;
 
-    for (i = 0; i < ARRAY_SIZE(xilinx_devcfg_regs_info); ++i) {
-        DepRegisterInfo *r = &s->regs_info[i];
-
-        *r = (DepRegisterInfo) {
-            .data = &s->regs[
-		    xilinx_devcfg_regs_info[i].decode.addr/4],
-            .data_size = sizeof(uint32_t),
-            .access = &xilinx_devcfg_regs_info[i],
-            .debug = XILINX_DEVCFG_ERR_DEBUG,
-            .prefix = prefix,
-            .opaque = s,
-        };
-        memory_region_init_io(&r->mem, OBJECT(dev), &devcfg_reg_ops, r,
-                              r->access->name, 4);
-        memory_region_add_subregion(&s->iomem, r->access->decode.addr, &r->mem);
-
-        if (s->dma_mr) {
-            s->dma_as = g_malloc0(sizeof *as);
-            address_space_init_shareable(s->dma_as, s->dma_mr, NULL)
-        } else {
-            s->dma_as = &address_space_memory;
-        }
+    if (s->dma_mr) {
+        s->dma_as = g_malloc0(sizeof *as);
+        address_space_init_shareable(s->dma_as, s->dma_mr, NULL)
+    } else {
+        s->dma_as = &address_space_memory;
     }
 }
 
@@ -433,10 +406,19 @@ static void xilinx_devcfg_init(Object *obj)
 {
     SysBusDevice *sbd = SYS_BUS_DEVICE(obj);
     XilinxDevcfg *s = XILINX_DEVCFG(obj);
+    RegisterInfoArray *reg_array;
 
     sysbus_init_irq(sbd, &s->irq);
 
     memory_region_init(&s->iomem, obj, "devcfg", R_MAX*4);
+    reg_array =
+        register_init_block32(DEVICE(obj), xilinx_devcfg_regs_info,
+                              ARRAY_SIZE(xilinx_devcfg_regs_info),
+                              s->regs_info, s->regs,
+                              &devcfg_reg_ops,
+                              XILINX_ZDMA_ERR_DEBUG,
+                              R_MAX * 4);
+    memory_region_add_subregion(&s->iomem, 0x0, &reg_array->mem);
     sysbus_init_mmio(sbd, &s->iomem);
 
     object_property_add_link(obj, "dma", TYPE_MEMORY_REGION,
