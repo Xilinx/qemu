27031b384148ba66fb5e01f1012fce42c98c40f2
Edgar E. Iglesias <edgar.iglesias@xilinx.com>
2020-03-19 18:08:15 +0100

    zynqmp: gic-proxy: Convert to use the upstream RegAPI
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- hw/intc/xilinx-zynqmp-gicp.c
+++ hw/intc/xilinx-zynqmp-gicp.c
@@ -27,7 +27,7 @@
 #include "qemu/osdep.h"
 #include "qapi/error.h"
 #include "hw/sysbus.h"
-#include "hw/register-dep.h"
+#include "hw/register.h"
 #include "migration/vmstate.h"
 #include "hw/qdev-properties.h"
 
@@ -49,21 +49,21 @@
 #define GICP_GROUPS         5
 #define GICP_GROUP_STRIDE   0x14
 
-DEP_REG32(GICP0_IRQ_STATUS, 0x0)
-DEP_REG32(GICP0_IRQ_MASK, 0x4)
-DEP_REG32(GICP0_IRQ_ENABLE, 0x8)
-DEP_REG32(GICP0_IRQ_DISABLE, 0xc)
-DEP_REG32(GICP0_IRQ_TRIGGER, 0x10)
+REG32(GICP0_IRQ_STATUS, 0x0)
+REG32(GICP0_IRQ_MASK, 0x4)
+REG32(GICP0_IRQ_ENABLE, 0x8)
+REG32(GICP0_IRQ_DISABLE, 0xc)
+REG32(GICP0_IRQ_TRIGGER, 0x10)
     #define R_GICP0_RSVD    0x000000ff
     #define R_GICP1_RSVD    0
     #define R_GICP2_RSVD    0
     #define R_GICP3_RSVD    0x000000ff
     #define R_GICP4_RSVD    0xf0000000
-DEP_REG32(GICP_PMU_IRQ_STATUS, 0xa0)
-DEP_REG32(GICP_PMU_IRQ_MASK, 0xa4)
-DEP_REG32(GICP_PMU_IRQ_ENABLE, 0xa8)
-DEP_REG32(GICP_PMU_IRQ_DISABLE, 0xac)
-DEP_REG32(GICP_PMU_IRQ_TRIGGER, 0xb0)
+REG32(GICP_PMU_IRQ_STATUS, 0xa0)
+REG32(GICP_PMU_IRQ_MASK, 0xa4)
+REG32(GICP_PMU_IRQ_ENABLE, 0xa8)
+REG32(GICP_PMU_IRQ_DISABLE, 0xac)
+REG32(GICP_PMU_IRQ_TRIGGER, 0xb0)
 
 #define R_MAX (R_GICP_PMU_IRQ_TRIGGER + 1)
 
@@ -74,7 +74,7 @@ typedef struct GICProxy {
 
     uint32_t pinState[GICP_GROUPS];
     uint32_t regs[R_MAX];
-    DepRegisterInfo regs_info[R_MAX];
+    RegisterInfo regs_info[R_MAX];
 } GICProxy;
 
 /* Mask and status registers are needed for checking if interrupt
@@ -108,7 +108,7 @@ static void gicp_update(void *opaque, uint8_t nr)
     gicp_update_irq(s);
 }
 
-static void gicp_status_postw(DepRegisterInfo *reg, uint64_t val64)
+static void gicp_status_postw(RegisterInfo *reg, uint64_t val64)
 {
     GICProxy *s = XILINX_GIC_PROXY(reg->opaque);
     unsigned int i;
@@ -118,7 +118,7 @@ static void gicp_status_postw(DepRegisterInfo *reg, uint64_t val64)
     }
 }
 
-static void gicp_enable_postw(DepRegisterInfo *reg, uint64_t val64)
+static void gicp_enable_postw(RegisterInfo *reg, uint64_t val64)
 {
     GICProxy *s = XILINX_GIC_PROXY(reg->opaque);
     uint32_t val = val64;
@@ -127,7 +127,7 @@ static void gicp_enable_postw(DepRegisterInfo *reg, uint64_t val64)
     gicp_update_irq(s);
 }
 
-static void gicp_disable_postw(DepRegisterInfo *reg, uint64_t val64)
+static void gicp_disable_postw(RegisterInfo *reg, uint64_t val64)
 {
     GICProxy *s = XILINX_GIC_PROXY(reg->opaque);
     uint32_t val = val64;
@@ -136,7 +136,7 @@ static void gicp_disable_postw(DepRegisterInfo *reg, uint64_t val64)
     gicp_update_irq(s);
 }
 
-static void gicp_trigger_postw(DepRegisterInfo *reg, uint64_t val64)
+static void gicp_trigger_postw(RegisterInfo *reg, uint64_t val64)
 {
     GICProxy *s = XILINX_GIC_PROXY(reg->opaque);
     uint32_t val = val64;
@@ -147,118 +147,114 @@ static void gicp_trigger_postw(DepRegisterInfo *reg, uint64_t val64)
 
 /* Functions for handling changes to each interrupt register. */
 
-static void gicpn_status_postw(DepRegisterInfo *reg, uint64_t val64)
+static void gicpn_status_postw(RegisterInfo *reg, uint64_t val64)
 {
     GICProxy *s = XILINX_GIC_PROXY(reg->opaque);
-    uint64_t nr = (uintptr_t)reg->access->opaque;
+    uint64_t nr = reg->access->addr / GICP_GROUP_STRIDE;
 
     s->regs[GICPN_STATUS_REG(nr)] |= s->pinState[nr];
     gicp_update(s, nr);
 }
 
-static void gicpn_enable_postw(DepRegisterInfo *reg, uint64_t val64)
+static void gicpn_enable_postw(RegisterInfo *reg, uint64_t val64)
 {
     GICProxy *s = XILINX_GIC_PROXY(reg->opaque);
     uint32_t val = val64;
-    uint64_t nr = (uintptr_t)reg->access->opaque;
+    uint64_t nr = reg->access->addr / GICP_GROUP_STRIDE;
 
     s->regs[GICPN_MASK_REG(nr)] &= ~val;
     gicp_update(s, nr);
 }
 
-static void gicpn_disable_postw(DepRegisterInfo *reg, uint64_t val64)
+static void gicpn_disable_postw(RegisterInfo *reg, uint64_t val64)
 {
     GICProxy *s = XILINX_GIC_PROXY(reg->opaque);
     uint32_t val = val64;
-    uint64_t nr = (uintptr_t)reg->access->opaque;
+    uint64_t nr = reg->access->addr / GICP_GROUP_STRIDE;
 
     s->regs[GICPN_MASK_REG(nr)] |= val;
     gicp_update(s, nr);
 }
 
-static void gicpn_trigger_postw(DepRegisterInfo *reg, uint64_t val64)
+static void gicpn_trigger_postw(RegisterInfo *reg, uint64_t val64)
 {
     GICProxy *s = XILINX_GIC_PROXY(reg->opaque);
     uint32_t val = val64;
-    uint64_t nr = (uintptr_t)reg->access->opaque;
+    uint64_t nr = reg->access->addr / GICP_GROUP_STRIDE;
 
     s->regs[GICPN_STATUS_REG(nr)] |= val;
     gicp_update(s, nr);
 }
 
 /* Return 0 and log if reading from write-only register. */
-static uint64_t gicp_wo_postr(DepRegisterInfo *reg, uint64_t val64)
+static uint64_t gicp_wo_postr(RegisterInfo *reg, uint64_t val64)
 {
     GICProxy *s = XILINX_GIC_PROXY(reg->opaque);
     qemu_log_mask(LOG_GUEST_ERROR,
                   "%s: Reading from wo register at %" HWADDR_PRIx "\n",
                   object_get_canonical_path(OBJECT(s)),
-                  reg->access->decode.addr);
+                  reg->access->addr);
     return 0;
 }
 
 /* Read-as-zero high 24 bits. */
-static uint64_t gicp_raz_hi24_postr(DepRegisterInfo *reg, uint64_t val64)
+static uint64_t gicp_raz_hi24_postr(RegisterInfo *reg, uint64_t val64)
 {
     return val64 & 0xff;
 }
 
-static DepRegisterAccessInfo gic_proxy_regs_info[] = {
+static RegisterAccessInfo gic_proxy_regs_info[] = {
 #define GICPN_REG_DEFS(n) \
     {   .name = "GICP" #n "_IRQ_STATUS",                            \
-        .decode.addr = A_GICP0_IRQ_STATUS + n * GICP_GROUP_STRIDE,  \
+        .addr = A_GICP0_IRQ_STATUS + n * GICP_GROUP_STRIDE,  \
         .w1c = 0xffffffff,                                          \
         .rsvd = R_GICP ## n ## _RSVD,                               \
         .post_write = gicpn_status_postw,                           \
-        .opaque = (void *)n,                                        \
     },{ .name = "GICP" #n "_IRQ_MASK",                              \
-        .decode.addr = A_GICP0_IRQ_MASK + n * GICP_GROUP_STRIDE,    \
+        .addr = A_GICP0_IRQ_MASK + n * GICP_GROUP_STRIDE,    \
         .ro = 0xffffffff,                                           \
         .rsvd = R_GICP ## n ## _RSVD,                               \
         .reset = 0xffffffff,                                        \
     },{ .name = "GICP" #n "_IRQ_ENABLE",                            \
-        .decode.addr = A_GICP0_IRQ_ENABLE + n * GICP_GROUP_STRIDE,  \
+        .addr = A_GICP0_IRQ_ENABLE + n * GICP_GROUP_STRIDE,  \
         .rsvd = R_GICP ## n ## _RSVD,                               \
         .post_read = gicp_wo_postr,                                 \
         .post_write = gicpn_enable_postw,                           \
-        .opaque = (void *)n,                                        \
     },{ .name = "GICP" #n "_IRQ_DISABLE",                           \
-        .decode.addr = A_GICP0_IRQ_DISABLE + n * GICP_GROUP_STRIDE, \
+        .addr = A_GICP0_IRQ_DISABLE + n * GICP_GROUP_STRIDE, \
         .rsvd = R_GICP ## n ## _RSVD,                               \
         .post_read = gicp_wo_postr,                                 \
         .post_write = gicpn_disable_postw,                          \
-        .opaque = (void *)n,                                        \
     },{ .name = "GICP" #n "_IRQ_TRIGGER",                           \
-        .decode.addr = A_GICP0_IRQ_TRIGGER + n * GICP_GROUP_STRIDE, \
+        .addr = A_GICP0_IRQ_TRIGGER + n * GICP_GROUP_STRIDE, \
         .rsvd = R_GICP ## n ## _RSVD,                               \
         .post_read = gicp_wo_postr,                                 \
         .post_write = gicpn_trigger_postw,                          \
-        .opaque = (void *)n,                                        \
     }
     GICPN_REG_DEFS(0),
     GICPN_REG_DEFS(1),
     GICPN_REG_DEFS(2),
     GICPN_REG_DEFS(3),
     GICPN_REG_DEFS(4),
-      { .name = "GICP_PMU_IRQ_STATUS",  .decode.addr = A_GICP_PMU_IRQ_STATUS,
+      { .name = "GICP_PMU_IRQ_STATUS",  .addr = A_GICP_PMU_IRQ_STATUS,
         .w1c = 0x000000ff,
         .rsvd = 0xffffffe0,
         .post_read = gicp_raz_hi24_postr,
         .post_write = gicp_status_postw,
-    },{ .name = "GICP_PMU_IRQ_MASK",  .decode.addr = A_GICP_PMU_IRQ_MASK,
+    },{ .name = "GICP_PMU_IRQ_MASK",  .addr = A_GICP_PMU_IRQ_MASK,
         .ro = 0x000000ff,
         .rsvd = 0xffffffe0,
         .reset = 0x000000ff,
         .post_read = gicp_raz_hi24_postr,
-    },{ .name = "GICP_PMU_IRQ_ENABLE",  .decode.addr = A_GICP_PMU_IRQ_ENABLE,
+    },{ .name = "GICP_PMU_IRQ_ENABLE",  .addr = A_GICP_PMU_IRQ_ENABLE,
         .rsvd = 0xffffffe0,
         .post_read = gicp_wo_postr,
         .post_write = gicp_enable_postw,
-    },{ .name = "GICP_PMU_IRQ_DISABLE",  .decode.addr = A_GICP_PMU_IRQ_DISABLE,
+    },{ .name = "GICP_PMU_IRQ_DISABLE",  .addr = A_GICP_PMU_IRQ_DISABLE,
         .rsvd = 0xffffffe0,
         .post_read = gicp_wo_postr,
         .post_write = gicp_disable_postw,
-    },{ .name = "GICP_PMU_IRQ_TRIGGER",  .decode.addr = A_GICP_PMU_IRQ_TRIGGER,
+    },{ .name = "GICP_PMU_IRQ_TRIGGER",  .addr = A_GICP_PMU_IRQ_TRIGGER,
         .rsvd = 0xffffffe0,
         .post_read = gicp_wo_postr,
         .post_write = gicp_trigger_postw,
@@ -271,72 +267,19 @@ static void gic_proxy_reset(DeviceState *dev)
     unsigned int i;
 
     for (i = 0; i < ARRAY_SIZE(s->regs_info); ++i) {
-        dep_register_reset(&s->regs_info[i]);
+        register_reset(&s->regs_info[i]);
     }
-}
-
-static uint64_t gic_proxy_read(void *opaque, hwaddr addr, unsigned size)
-{
-    GICProxy *s = XILINX_GIC_PROXY(opaque);
-    DepRegisterInfo *r = &s->regs_info[addr / 4];
-
-    if (!r->data) {
-        qemu_log_mask(LOG_GUEST_ERROR,
-                      "%s: Decode error: read from %" HWADDR_PRIx "\n",
-                      object_get_canonical_path(OBJECT(s)),
-                      addr);
-        return 0;
-    }
-    return dep_register_read(r);
-}
-
-static void gic_proxy_write(void *opaque, hwaddr addr, uint64_t value,
-                      unsigned size)
-{
-    GICProxy *s = XILINX_GIC_PROXY(opaque);
-    DepRegisterInfo *r = &s->regs_info[addr / 4];
-
-    if (!r->data) {
-        qemu_log_mask(LOG_GUEST_ERROR,
-                      "%s: Decode error: write %" HWADDR_PRIx "=%" PRIx64 "\n",
-                      object_get_canonical_path(OBJECT(s)),
-                      addr, value);
-        return;
-    }
-    dep_register_write(r, value, ~0);
 }
 
 static const MemoryRegionOps gic_proxy_ops = {
-    .read = gic_proxy_read,
-    .write = gic_proxy_write,
+    .read = register_read_memory,
+    .write = register_write_memory,
     .endianness = DEVICE_LITTLE_ENDIAN,
     .valid = {
         .min_access_size = 4,
         .max_access_size = 4,
     },
 };
-
-static void gic_proxy_realize(DeviceState *dev, Error **errp)
-{
-    GICProxy *s = XILINX_GIC_PROXY(dev);
-    const char *prefix = object_get_canonical_path(OBJECT(dev));
-    unsigned int i;
-
-    for (i = 0; i < ARRAY_SIZE(gic_proxy_regs_info); ++i) {
-        DepRegisterInfo *r = &s->regs_info[gic_proxy_regs_info[i].decode.addr/4];
-
-        *r = (DepRegisterInfo) {
-            .data = (uint8_t *)&s->regs[
-                    gic_proxy_regs_info[i].decode.addr/4],
-            .data_size = sizeof(uint32_t),
-            .access = &gic_proxy_regs_info[i],
-            .debug = GIC_PROXY_ERR_DEBUG,
-            .prefix = prefix,
-            .opaque = s,
-        };
-        dep_register_init(r);
-    }
-}
 
 static void gic_proxy_set_irq(void *opaque, int irq, int level)
 {
@@ -358,6 +301,19 @@ static void gic_proxy_init(Object *obj)
     GICProxy *s = XILINX_GIC_PROXY(obj);
     SysBusDevice *sbd = SYS_BUS_DEVICE(obj);
     DeviceState *dev = DEVICE(s);
+    RegisterInfoArray *reg_array;
+
+    memory_region_init(&s->iomem, obj, TYPE_XILINX_GIC_PROXY, R_MAX * 4);
+    reg_array =
+        register_init_block32(DEVICE(obj), gic_proxy_regs_info,
+                              ARRAY_SIZE(gic_proxy_regs_info),
+                              s->regs_info, s->regs,
+                              &gic_proxy_ops,
+                              GIC_PROXY_ERR_DEBUG,
+                              R_MAX * 4);
+    memory_region_add_subregion(&s->iomem,
+                                0x0,
+                                &reg_array->mem);
 
     /* IRQ grouping:
      * [0..31] - GICP0
@@ -369,8 +325,6 @@ static void gic_proxy_init(Object *obj)
     qdev_init_gpio_in(dev, gic_proxy_set_irq, MAX_INTS);
     qdev_init_gpio_out_named(dev, &s->irq, "gicp-irq", 1);
 
-    memory_region_init_io(&s->iomem, obj, &gic_proxy_ops, s,
-                          TYPE_XILINX_GIC_PROXY, R_MAX * 4);
     sysbus_init_mmio(sbd, &s->iomem);
     sysbus_init_irq(sbd, &s->irq);
 }
@@ -433,7 +387,6 @@ static void gic_proxy_class_init(ObjectClass *oc, void *data)
     FDTGenericGPIOClass *fggc = FDT_GENERIC_GPIO_CLASS(oc);
 
     dc->reset = gic_proxy_reset;
-    dc->realize = gic_proxy_realize;
     dc->vmsd = &vmstate_gic_proxy;
     fgic->get_irq = gic_proxy_get_irq;
     fggc->client_gpios = gic_proxy_client_gpios;
