7ce8019669c454c838a31ca43a3f32b0a9f5de25
Sakis Panou <sakis.panou@xilinx.com>
2020-03-31 19:35:57 -0700

    Revert "hw/rtc: Updated  the RTC module following v1 internal review."
    This reverts commit 6a3577923ed8aec9a4026f44c6edd0b9045a54a3.

--- hw/rtc/xlnx-zynqmp-rtc.c
+++ hw/rtc/xlnx-zynqmp-rtc.c
@@ -25,7 +25,6 @@
  */
 
 #include "qemu/osdep.h"
-#include <stdint.h>
 #include "qemu-common.h"
 #include "hw/sysbus.h"
 #include "hw/register.h"
@@ -39,29 +38,9 @@
 #include "hw/rtc/xlnx-zynqmp-rtc.h"
 #include "migration/vmstate.h"
 #include "hw/qdev-properties.h"
-#include "qemu/timer.h"
 
 #ifndef XLNX_ZYNQMP_RTC_ERR_DEBUG
 #define XLNX_ZYNQMP_RTC_ERR_DEBUG 0
-#endif
-
-#if XLNX_ZYNQMP_RTC_ERR_DEBUG
-    #define DPRINT(fmt, args...) do { \
-            if (XLNX_ZYNQMP_RTC_ERR_DEBUG) { \
-                qemu_log(fmt, ##args); \
-            } \
-        } while (0)
-
-    #define DPRINT_TM(fmt, args...) do { \
-            if (XLNX_ZYNQMP_RTC_ERR_DEBUG) { \
-                qemu_log("[%lld] -> " fmt,\
-                        qemu_clock_get_ns(rtc_clock) / NANOSECONDS_PER_SECOND,\
-                        ##args);\
-            } \
-        } while (0)
-#else
-    #define DPRINT(fmt, args...) do {} while (0)
-    #define DPRINT_TM(fmt, args...) do {} while (0)
 #endif
 
 enum version_id {
@@ -74,7 +53,7 @@ struct version_item_lookup {
     const char *str;
 };
 
-static struct version_item_lookup version_table_lookup[] = {
+static struct version_item_lookup  version_table_lookup[] = {
     { IP_VERSION_1_0_1, "1.0.1" },
     { IP_VERSION_2_0_0, "2.0.0" }
 };
@@ -84,146 +63,10 @@ static Property xlnx_rtc_properties[] = {
     DEFINE_PROP_END_OF_LIST(),
 };
 
-/* Returns the current host time in seconds */
-static uint32_t get_host_time_now(void)
+static void rtc_int_update_irq(XlnxZynqMPRTC *s)
 {
-    int64_t host_time_now = qemu_clock_get_ns(rtc_clock);
-    return host_time_now / NANOSECONDS_PER_SECOND;
-}
-
-/* Returns the qemu time (time set with the -rtc command line) in seconds */
-static uint32_t get_qemu_time_now(XlnxZynqMPRTC *s)
-{
-    return get_host_time_now() - s->tick_offset;
-}
-
-/* Return the guest time in seconds */
-static uint32_t get_guest_time_now(XlnxZynqMPRTC *s)
-{
-    return get_qemu_time_now(s) - s->guest_offset;
-}
-
-/*Return the designated Guest Time in Host time*/
-static uint32_t host_time_from_guest(XlnxZynqMPRTC *s, uint32_t guest_time)
-{
-    return s->tick_offset + s->guest_offset + guest_time;
-}
-
-static void update_alarm(XlnxZynqMPRTC *s)
-{
-    uint32_t alarm;
-    uint32_t host_time_now;
-
-    timer_del(s->alarm);
-    /*
-     * Converts the guest alarm time to a host alarm time as all internal
-     * QEMUTimers are based on host time, this will also take care of all
-     * overflows.
-     */
-    alarm = host_time_from_guest(s, s->regs[R_ALARM]);
-    host_time_now = get_host_time_now();
-
-    /*
-     * If the alarm time is earlier than the current host time the timer
-     * callback will be called instantaneously. To avoid this we will only
-     * arm the timer if the alarm value is at a time later than the current
-     * host time. Conversion from Guest Time to Host time is taken care of
-     * by the call to host_time_from_guest().
-     */
-    if (alarm > host_time_now) {
-        timer_mod(s->alarm, alarm * NANOSECONDS_PER_SECOND);
-    } else if (alarm == host_time_now) {
-        s->regs[R_RTC_INT_STATUS] = FIELD_DP32(s->regs[R_RTC_INT_STATUS],
-                                               RTC_INT_STATUS, ALARM, 1);
-
-        if (FIELD_EX32(s->regs[R_RTC_INT_MASK], RTC_INT_MASK, ALARM) == 0) {
-            qemu_set_irq(s->irq_rtc_int, true);
-        }
-    } else {
-        qemu_log_mask(LOG_GUEST_ERROR, "%06d : %s() attempting to arm the "
-                      "alarm timer with a timestamp that is earlier than "
-                      "the current time: \talarm=%u,\n\tguest time=%u,\n",
-                      __LINE__, __func__, alarm, get_guest_time_now(s));
-    }
-}
-
-static void update_seconds(XlnxZynqMPRTC *s)
-{
-    uint32_t next_sec;
-
-    timer_del(s->sec_tick);
-    /* Re-arm the seconds tick and go */
-    next_sec = get_host_time_now() + 1;
-    timer_mod(s->sec_tick, next_sec * NANOSECONDS_PER_SECOND);
-}
-
-static void rtc_set_time_write_postw(RegisterInfo *reg, uint64_t val64)
-{
-    XlnxZynqMPRTC *s = XLNX_ZYNQMP_RTC(reg->opaque);
-
-    s->regs[R_SET_TIME_WRITE] = val64;
-    /* Will force to read the last value written as per controller spec.*/
-    s->regs[R_SET_TIME_READ] = val64;
-    /* Update the guest offset to reflect the new time set. */
-    s->guest_offset = get_qemu_time_now(s) - s->regs[R_SET_TIME_READ];
-    DPRINT_TM("%06d : %s()\n", __LINE__, __func__);
-    DPRINT_TM("Time Marks:\n\tQEMU Time = %u,\n \tHost Time = %u,\n"
-              " \ts->tick_offset = %u,\n",
-              get_qemu_time_now(s), get_host_time_now(), s->tick_offset);
-    DPRINT("\tguest_offset = %010u\n", s->guest_offset);
-}
-
-static void rtc_calib_write_postw(RegisterInfo *reg, uint64_t val64)
-{
-    XlnxZynqMPRTC *s = XLNX_ZYNQMP_RTC(reg->opaque);
-
-    s->regs[R_CALIB_READ] = val64;
-    /*
-     * Since we are not simulating calibration, force CURRENT_TICK
-     * to always read the MAX_TICK
-     */
-    s->regs[R_CURRENT_TICK] = FIELD_EX32(s->regs[R_CALIB_READ],
-                                       CALIB_WRITE, MAX_TICK);
-}
-
-static uint64_t current_time_postr(RegisterInfo *reg, uint64_t val64)
-{
-    XlnxZynqMPRTC *s = XLNX_ZYNQMP_RTC(reg->opaque);
-    uint32_t guest_time_now = get_guest_time_now(s);
-
-    return guest_time_now;
-}
-
-static void alarm_postw(RegisterInfo *reg, uint64_t val64)
-{
-    XlnxZynqMPRTC *s = XLNX_ZYNQMP_RTC(reg->opaque);
-
-    s->regs[R_ALARM] = val64;
-    update_alarm(s);
-}
-
-static uint64_t rtc_int_en_prew(RegisterInfo *reg, uint64_t val64)
-{
-    XlnxZynqMPRTC *s = XLNX_ZYNQMP_RTC(reg->opaque);
-
-    s->regs[R_RTC_INT_MASK] &= ~val64;
-    return 0;
-}
-
-static uint64_t rtc_int_dis_prew(RegisterInfo *reg, uint64_t val64)
-{
-    XlnxZynqMPRTC *s = XLNX_ZYNQMP_RTC(reg->opaque);
-
-    s->regs[R_RTC_INT_MASK] |= val64;
-    return 0;
-}
-
-static void rtc_int_status_postw(RegisterInfo *reg, uint64_t val64)
-{
-    XlnxZynqMPRTC *s = XLNX_ZYNQMP_RTC(reg->opaque);
-
-    update_alarm(s);
-    update_seconds(s);
+    bool pending = s->regs[R_RTC_INT_STATUS] & ~s->regs[R_RTC_INT_MASK];
+    qemu_set_irq(s->irq_rtc_int, pending);
 }
 
 static void addr_error_int_update_irq(XlnxZynqMPRTC *s)
@@ -232,14 +75,41 @@ static void addr_error_int_update_irq(XlnxZynqMPRTC *s)
     qemu_set_irq(s->irq_addr_error_int, pending);
 }
 
-static void addr_error_set_status(void *opaque)
+static uint32_t rtc_get_count(XlnxZynqMPRTC *s)
 {
-    RegisterInfo *ri = *((RegisterInfo **)((RegisterInfoArray*)opaque)->r);
-    XlnxZynqMPRTC *s = (XlnxZynqMPRTC *)ri->opaque;
+    int64_t now = qemu_clock_get_ns(rtc_clock);
+    return s->tick_offset + now / NANOSECONDS_PER_SECOND;
+}
 
-    s->regs[R_ADDR_ERROR] = FIELD_DP32(s->regs[R_ADDR_ERROR],
-                                       ADDR_ERROR, STATUS, 1);
-    addr_error_int_update_irq(s);
+static uint64_t current_time_postr(RegisterInfo *reg, uint64_t val64)
+{
+    XlnxZynqMPRTC *s = XLNX_ZYNQMP_RTC(reg->opaque);
+
+    return rtc_get_count(s);
+}
+
+static void rtc_int_status_postw(RegisterInfo *reg, uint64_t val64)
+{
+    XlnxZynqMPRTC *s = XLNX_ZYNQMP_RTC(reg->opaque);
+    rtc_int_update_irq(s);
+}
+
+static uint64_t rtc_int_en_prew(RegisterInfo *reg, uint64_t val64)
+{
+    XlnxZynqMPRTC *s = XLNX_ZYNQMP_RTC(reg->opaque);
+
+    s->regs[R_RTC_INT_MASK] &= (uint32_t) ~val64;
+    rtc_int_update_irq(s);
+    return 0;
+}
+
+static uint64_t rtc_int_dis_prew(RegisterInfo *reg, uint64_t val64)
+{
+    XlnxZynqMPRTC *s = XLNX_ZYNQMP_RTC(reg->opaque);
+
+    s->regs[R_RTC_INT_MASK] |= (uint32_t) val64;
+    rtc_int_update_irq(s);
+    return 0;
 }
 
 static void addr_error_postw(RegisterInfo *reg, uint64_t val64)
@@ -252,7 +122,7 @@ static uint64_t addr_error_int_en_prew(RegisterInfo *reg, uint64_t val64)
 {
     XlnxZynqMPRTC *s = XLNX_ZYNQMP_RTC(reg->opaque);
 
-    s->regs[R_ADDR_ERROR_INT_MASK] &= ~val64;
+    s->regs[R_ADDR_ERROR_INT_MASK] &= (uint32_t) ~val64;
     addr_error_int_update_irq(s);
     return 0;
 }
@@ -261,52 +131,63 @@ static uint64_t addr_error_int_dis_prew(RegisterInfo *reg, uint64_t val64)
 {
     XlnxZynqMPRTC *s = XLNX_ZYNQMP_RTC(reg->opaque);
 
-    s->regs[R_ADDR_ERROR_INT_MASK] |= val64;;
+    s->regs[R_ADDR_ERROR_INT_MASK] |= (uint32_t) val64;
     addr_error_int_update_irq(s);
     return 0;
 }
 
+static void rtc_set_timer_postw(RegisterInfo *reg, uint64_t val64)
+{
+    XlnxZynqMPRTC *s = XLNX_ZYNQMP_RTC(reg->opaque);
+    s->tick_offset = (uint32_t) val64;
+}
+
+static void rtc_calib_write_postw(RegisterInfo *reg, uint64_t val64)
+{
+    XlnxZynqMPRTC *s = XLNX_ZYNQMP_RTC(reg->opaque);
+    s->regs[R_CALIB_READ] = (uint32_t) val64;
+}
+
 static const RegisterAccessInfo rtc_regs_info[] = {
-    {   .name = "SET_TIME_WRITE", .addr = A_SET_TIME_WRITE,
-        .post_write = rtc_set_time_write_postw,
-    },{ .name = "SET_TIME_READ", .addr = A_SET_TIME_READ,
-        .ro = 0xffffffff,
-    },{ .name = "CALIB_WRITE", .addr = A_CALIB_WRITE,
-        .post_write = rtc_calib_write_postw,
-    },{ .name = "CALIB_READ", .addr = A_CALIB_READ,
-        .ro = 0x1fffff,
-    },{ .name = "CURRENT_TIME", .addr = A_CURRENT_TIME,
+    {   .name = "SET_TIME_WRITE",  .addr = A_SET_TIME_WRITE,
+        .post_write = rtc_set_timer_postw,
+    },{ .name = "SET_TIME_READ",  .addr = A_SET_TIME_READ,
         .ro = 0xffffffff,
         .post_read = current_time_postr,
-    },{ .name = "CURRENT_TICK", .addr = A_CURRENT_TICK,
+    },{ .name = "CALIB_WRITE",  .addr = A_CALIB_WRITE,
+        .post_write = rtc_calib_write_postw,
+    },{ .name = "CALIB_READ",  .addr = A_CALIB_READ,
+        .ro = 0x1fffff,
+    },{ .name = "CURRENT_TIME",  .addr = A_CURRENT_TIME,
+        .ro = 0xffffffff,
+        .post_read = current_time_postr,
+    },{ .name = "CURRENT_TICK",  .addr = A_CURRENT_TICK,
         .ro = 0xffff,
-    },{ .name = "ALARM", .addr = A_ALARM,
-        .post_write = alarm_postw,
-        .reset = 0x00000000,
-    },{ .name = "RTC_INT_STATUS", .addr = A_RTC_INT_STATUS,
+    },{ .name = "ALARM",  .addr = A_ALARM,
+    },{ .name = "RTC_INT_STATUS",  .addr = A_RTC_INT_STATUS,
         .w1c = 0x3,
         .post_write = rtc_int_status_postw,
-    },{ .name = "RTC_INT_MASK", .addr = A_RTC_INT_MASK,
+    },{ .name = "RTC_INT_MASK",  .addr = A_RTC_INT_MASK,
         .reset = 0x3,
         .ro = 0x3,
-    },{ .name = "RTC_INT_EN", .addr = A_RTC_INT_EN,
+    },{ .name = "RTC_INT_EN",  .addr = A_RTC_INT_EN,
         .pre_write = rtc_int_en_prew,
-    },{ .name = "RTC_INT_DIS", .addr = A_RTC_INT_DIS,
+    },{ .name = "RTC_INT_DIS",  .addr = A_RTC_INT_DIS,
         .pre_write = rtc_int_dis_prew,
-    },{ .name = "ADDR_ERROR", .addr = A_ADDR_ERROR,
+    },{ .name = "ADDR_ERROR",  .addr = A_ADDR_ERROR,
         .w1c = 0x1,
         .post_write = addr_error_postw,
-    },{ .name = "ADDR_ERROR_INT_MASK", .addr = A_ADDR_ERROR_INT_MASK,
+    },{ .name = "ADDR_ERROR_INT_MASK",  .addr = A_ADDR_ERROR_INT_MASK,
         .reset = 0x1,
         .ro = 0x1,
-    },{ .name = "ADDR_ERROR_INT_EN", .addr = A_ADDR_ERROR_INT_EN,
+    },{ .name = "ADDR_ERROR_INT_EN",  .addr = A_ADDR_ERROR_INT_EN,
         .pre_write = addr_error_int_en_prew,
-    },{ .name = "ADDR_ERROR_INT_DIS", .addr = A_ADDR_ERROR_INT_DIS,
+    },{ .name = "ADDR_ERROR_INT_DIS",  .addr = A_ADDR_ERROR_INT_DIS,
         .pre_write = addr_error_int_dis_prew,
-    },{ .name = "CONTROL", .addr = A_CONTROL,
+    },{ .name = "CONTROL",  .addr = A_CONTROL,
         .reset = 0x1000000,
         .rsvd = 0x70fffffe,
-    },{ .name = "SAFETY_CHK", .addr = A_SAFETY_CHK,
+    },{ .name = "SAFETY_CHK",  .addr = A_SAFETY_CHK,
     }
 };
 
@@ -319,61 +200,19 @@ static enum version_id version_id_lookup(const char *str)
 {
     uint32_t i;
     enum version_id version;
+
     version = IP_VERSION_1_0_1;
 
     if (str) {
         for (i = 0; i < ARRAY_SIZE(version_table_lookup); ++i) {
             if (!strcmp(str, version_table_lookup[i].str)) {
-                version = version_table_lookup[i].id;
+                version =  version_table_lookup[i].id;
                 break;
             }
         }
     }
 
     return version;
-}
-
-static void clear_time(XlnxZynqMPRTC *s)
-{
-    time_t qemu_time;
-    /*
-     * Host Time is determined by the host clock, the QEMU RTC clock ticks
-     * off from.
-     */
-    uint32_t host_time = get_host_time_now();
-    /*
-     * QEMU Time is determined by the ISO8601 value passed to QEMU
-     * in the command line using the -rtc command line option. If
-     * the user omits the -rtc command line option then QEMU Time
-     * is equal to the Host Time.
-     */
-    struct tm qemu_tm = {0};
-    qemu_get_timedate(&qemu_tm, 0);
-    qemu_time = mktimegm(&qemu_tm);
-    /*
-     * tick_offset tracks the delta in seconds between the Host Time and
-     * QEMU Time.
-     */
-    s->tick_offset = host_time - qemu_time;
-    /*
-     * The Guest Time is the time set by the guest, to begin with we'll use
-     * the QEMU Time as the Guest Time as this is what was passed at command
-     * line. We'll apply the QEMU Time to the Guest Set Time Read/Write
-     * registers. The Guest can change that by writing to the Set Time Write
-     * Register.
-     */
-    s->regs[R_SET_TIME_WRITE] = qemu_time;
-    s->regs[R_SET_TIME_READ] = qemu_time;
-    s->guest_offset = qemu_time - s->regs[R_SET_TIME_READ];
-
-    DPRINT_TM("%06d : %s()\n", __LINE__, __func__);
-    DPRINT_TM("Time Marks:\n\tQEMU Time = %lu,\n\tHost Time = %u,\n"
-              " \ts->tick_offset = %u,\n",
-              qemu_time, host_time, s->tick_offset);
-    DPRINT("\tguest_offset = %010u\n", s->guest_offset);
-    DPRINT("\t%04u-%02u-%02u-T%02u:%02u:%02u (yyyy-mm-ddThh:mm:ss ISO-8601)\n",
-              qemu_tm.tm_year + 1900, qemu_tm.tm_mon + 1, qemu_tm.tm_mday,
-              qemu_tm.tm_hour, qemu_tm.tm_min, qemu_tm.tm_sec);
 }
 
 static void rtc_reset(DeviceState *dev)
@@ -390,64 +229,17 @@ static void rtc_reset(DeviceState *dev)
         register_reset(&s->regs_info[R_CONTROL]);
     }
 
-    clear_time(s);
-}
+    trace_xlnx_zynqmp_rtc_gettime(s->current_tm.tm_year, s->current_tm.tm_mon,
+                                  s->current_tm.tm_mday, s->current_tm.tm_hour,
+                                  s->current_tm.tm_min, s->current_tm.tm_sec);
 
-static void alarm_timeout_cb(void *opaque)
-{
-    XlnxZynqMPRTC *s = (XlnxZynqMPRTC *)opaque;
-
-    s->regs[R_RTC_INT_STATUS] = FIELD_DP32(s->regs[R_RTC_INT_STATUS],
-        RTC_INT_STATUS, ALARM, 1);
-
-    if (FIELD_EX32(s->regs[R_RTC_INT_MASK], RTC_INT_MASK, ALARM) == 0) {
-        qemu_set_irq(s->irq_rtc_int, true);
-    }
-}
-
-static void second_timeout_cb(void *opaque)
-{
-    XlnxZynqMPRTC *s = (XlnxZynqMPRTC *)opaque;
-
-    s->regs[R_RTC_INT_STATUS] = FIELD_DP32(s->regs[R_RTC_INT_STATUS],
-        RTC_INT_STATUS, SECONDS, 1);
-
-    if (FIELD_EX32(s->regs[R_RTC_INT_MASK], RTC_INT_MASK, SECONDS) == 0) {
-        qemu_set_irq(s->irq_rtc_int, true);
-    }
-}
-
-static uint64_t rtc_register_read_memory(void *opaque, hwaddr addr,
-                                         unsigned size)
-{
-    if (addr >= (XLNX_ZYNQMP_RTC_R_MAX * 4)) {
-        DPRINT_TM("%06d : %s()\n", __LINE__, __func__);
-        DPRINT_TM("\tAttempting to Read from invalid RTC Memory Space 0x%08x\n",
-                  addr);
-        addr_error_set_status(opaque);
-        return (uint64_t)0;
-    }
-
-    return register_read_memory(opaque, addr, size);
-}
-
-static void rtc_register_write_memory(void *opaque, hwaddr addr, uint64_t value,
-                                      unsigned size)
-{
-    if (addr >= (XLNX_ZYNQMP_RTC_R_MAX * 4)) {
-        DPRINT_TM("%06d : %s()\n", __LINE__, __func__);
-        DPRINT_TM("\tAttempting to Write to invalid RTC Memory Space 0x%08x\n",
-                  addr);
-        addr_error_set_status(opaque);
-        return;
-    }
-
-    register_write_memory(opaque, addr, value, size);
+    rtc_int_update_irq(s);
+    addr_error_int_update_irq(s);
 }
 
 static const MemoryRegionOps rtc_ops = {
-    .read = rtc_register_read_memory,
-    .write = rtc_register_write_memory,
+    .read = register_read_memory,
+    .write = register_write_memory,
     .endianness = DEVICE_LITTLE_ENDIAN,
     .valid = {
         .min_access_size = 4,
@@ -460,30 +252,31 @@ static void rtc_init(Object *obj)
     XlnxZynqMPRTC *s = XLNX_ZYNQMP_RTC(obj);
     SysBusDevice *sbd = SYS_BUS_DEVICE(obj);
     RegisterInfoArray *reg_array;
+    struct tm current_tm;
 
     memory_region_init(&s->iomem, obj, TYPE_XLNX_ZYNQMP_RTC,
-                       XLNX_ZYNQMP_RTC_IO_REGION_SZ);
-
+                       XLNX_ZYNQMP_RTC_R_MAX * 4);
     reg_array =
         register_init_block32(DEVICE(obj), rtc_regs_info,
                               ARRAY_SIZE(rtc_regs_info),
                               s->regs_info, s->regs,
                               &rtc_ops,
                               XLNX_ZYNQMP_RTC_ERR_DEBUG,
-                              XLNX_ZYNQMP_RTC_IO_REGION_SZ);
-
+                              XLNX_ZYNQMP_RTC_R_MAX * 4);
     memory_region_add_subregion(&s->iomem,
                                 0x0,
                                 &reg_array->mem);
-
     sysbus_init_mmio(sbd, &s->iomem);
     sysbus_init_irq(sbd, &s->irq_rtc_int);
     sysbus_init_irq(sbd, &s->irq_addr_error_int);
 
-    DPRINT_TM("%06d : %s()\n", __LINE__, __func__);
-    clear_time(s);
-    s->alarm = timer_new_ns(rtc_clock, alarm_timeout_cb, s);
-    s->sec_tick = timer_new_ns(rtc_clock, second_timeout_cb, s);
+    qemu_get_timedate(&current_tm, 0);
+    s->tick_offset = mktimegm(&current_tm) -
+        qemu_clock_get_ns(rtc_clock) / NANOSECONDS_PER_SECOND;
+
+    trace_xlnx_zynqmp_rtc_gettime(current_tm.tm_year, current_tm.tm_mon,
+                                  current_tm.tm_mday, current_tm.tm_hour,
+                                  current_tm.tm_min, current_tm.tm_sec);
 }
 
 static int rtc_pre_save(void *opaque)
@@ -493,6 +286,7 @@ static int rtc_pre_save(void *opaque)
 
     /* Add the time at migration */
     s->tick_offset = s->tick_offset + now;
+
     return 0;
 }
 
@@ -506,6 +300,7 @@ static int rtc_post_load(void *opaque, int version_id)
      * stopped to the offset.
      */
     s->tick_offset = s->tick_offset - now;
+
     return 0;
 }
 
@@ -525,6 +320,7 @@ static const VMStateDescription vmstate_rtc = {
 static void rtc_class_init(ObjectClass *klass, void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(klass);
+
     dc->reset = rtc_reset;
     dc->props = xlnx_rtc_properties;
     dc->vmsd = &vmstate_rtc;
