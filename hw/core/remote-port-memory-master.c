a8e64ad07a866f162ef27a85d4f692427b666955
Edgar E. Iglesias <edgar.iglesias@xilinx.com>
2020-05-18 08:41:46 +0200

    remote-port: memory-master: Remove stray semi-colon
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- hw/core/remote-port-memory-master.c
+++ hw/core/remote-port-memory-master.c
@@ -33,7 +33,7 @@
         fprintf(stderr,  ": %s: ", __func__); \
         fprintf(stderr, ## __VA_ARGS__); \
     } \
-} while (0);
+} while (0)
 
 #define TYPE_REMOTE_PORT_MEMORY_MASTER "remote-port-memory-master"
 #define REMOTE_PORT_MEMORY_MASTER(obj) \
