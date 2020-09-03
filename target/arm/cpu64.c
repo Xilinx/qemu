80199bf83bb45b005b1ed2f4a020e0911284afda
Edgar E. Iglesias <edgar.iglesias@xilinx.com>
2020-09-03 18:55:09 +0200

    target/arm: Add Cortex-A78
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- target/arm/cpu64.c
+++ target/arm/cpu64.c
@@ -262,6 +262,66 @@ static void aarch64_a72_initfn(Object *obj)
     cpu->isar.id_aa64pfr0 |= (uint64_t)1 << 60; /* KPTI.  */
 }
 
+static void aarch64_a78_initfn(Object *obj)
+{
+    ARMCPU *cpu = ARM_CPU(obj);
+
+    cpu->dtb_compatible = "arm,cortex-a78";
+    set_feature(&cpu->env, ARM_FEATURE_V8);
+    set_feature(&cpu->env, ARM_FEATURE_NEON);
+    set_feature(&cpu->env, ARM_FEATURE_GENERIC_TIMER);
+    set_feature(&cpu->env, ARM_FEATURE_AARCH64);
+    set_feature(&cpu->env, ARM_FEATURE_CBAR_RO);
+    set_feature(&cpu->env, ARM_FEATURE_EL2);
+    set_feature(&cpu->env, ARM_FEATURE_EL3);
+    set_feature(&cpu->env, ARM_FEATURE_PMU);
+    cpu->midr = 0x410fd421;
+    cpu->revidr = 0x00000000;
+    cpu->reset_fpsid = 0x41034080;
+    cpu->isar.mvfr0 = 0x10110222;
+    cpu->isar.mvfr1 = 0x12111111;
+    cpu->isar.mvfr2 = 0x00000043;
+    cpu->ctr = 0x8444c004;
+    cpu->reset_sctlr = 0x00c50838;
+    cpu->id_pfr0 = 0x00000131;
+    cpu->id_pfr1 = 0x00011011;
+    cpu->isar.id_dfr0 = 0x03010066;
+    cpu->id_afr0 = 0x00000000;
+    cpu->isar.id_mmfr0 = 0x10201105;
+    cpu->isar.id_mmfr1 = 0x40000000;
+    cpu->isar.id_mmfr2 = 0x01260000;
+    cpu->isar.id_mmfr3 = 0x02122211;
+    cpu->isar.id_isar0 = 0x02101110;
+    cpu->isar.id_isar1 = 0x13112111;
+    cpu->isar.id_isar2 = 0x21232042;
+    cpu->isar.id_isar3 = 0x01112131;
+    cpu->isar.id_isar4 = 0x00010142;
+    cpu->isar.id_isar5 = 0x00011121;
+    cpu->isar.id_isar6 = 0x00000010;
+    /* TOP Bit zero until we implement RAS.  */
+    cpu->isar.id_aa64pfr0 = 0x01111112;
+    cpu->isar.id_aa64pfr1 = 0x00000010;
+    cpu->isar.id_aa64dfr0 = 0x110305408ULL;
+    cpu->isar.id_aa64isar0 = 0x0010100010211120ULL;
+    cpu->isar.id_aa64isar1 = 0x01200031;
+    cpu->isar.id_aa64mmfr0 = 0x000101125;
+    cpu->isar.dbgdidr = 0x3516d000;
+    cpu->clidr = 0x0c300023;
+    cpu->ccsidr[0] = 0x701fe00a; /* 32KB L1 dcache */
+    cpu->ccsidr[1] = 0x201fe012; /* 48KB L1 icache */
+    cpu->ccsidr[2] = 0x707fe07a; /* 1MB L2 cache */
+    cpu->dcz_blocksize = 4; /* 64 bytes */
+    cpu->gic_num_lrs = 4;
+    cpu->gic_vpribits = 5;
+    cpu->gic_vprebits = 5;
+    define_arm_cp_regs(cpu, cortex_a72_a57_a53_cp_reginfo);
+
+    /* Xilinx FIXUPs.  */
+    /* These indicate the BP hardening and KPTI aren't needed.  */
+    cpu->isar.id_aa64pfr0 |= (uint64_t)1 << 56; /* BP.  */
+    cpu->isar.id_aa64pfr0 |= (uint64_t)1 << 60; /* KPTI.  */
+}
+
 void arm_cpu_sve_finalize(ARMCPU *cpu, Error **errp)
 {
     /*
@@ -865,6 +925,7 @@ static const ARMCPUInfo aarch64_cpus[] = {
     { .name = "cortex-a57",         .initfn = aarch64_a57_initfn },
     { .name = "cortex-a53",         .initfn = aarch64_a53_initfn },
     { .name = "cortex-a72",         .initfn = aarch64_a72_initfn },
+    { .name = "cortex-a78",         .initfn = aarch64_a78_initfn },
     { .name = "max",                .initfn = aarch64_max_initfn },
 };
 
