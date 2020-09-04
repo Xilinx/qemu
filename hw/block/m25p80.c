1eb7d50646fb8f481893135986ca8028c1f6e732
Sai Pavan Boddu <sai.pavan.boddu@xilinx.com>
2020-09-04 16:36:27 +0530

    block/m25p80: Add micro octal 2Gib part
    Add support for MT35XU02GBBA 2Gib part.
    
    Signed-off-by: Sai Pavan Boddu <sai.pavan.boddu@xilinx.com>
    Message-Id: <1599217587-29369-1-git-send-email-sai.pavan.boddu@xilinx.com>
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- hw/block/m25p80.c
+++ hw/block/m25p80.c
@@ -237,6 +237,7 @@ static const FlashPartInfo known_devices[] = {
     { INFO("n25q512a13",  0x20ba20,      0,  64 << 10, 1024, ER_4K) },
     { INFO("m25qu02gcbb", 0x20bb22,      0,  64 << 10, 4096, ER_4K) },
     { INFO("mt35xu01gbba", 0x2c5b1b, 0x104100, 128 << 10, 1024, ER_4K) },
+    { INFO("mt35xu02gbba", 0x2c5b1c,     0, 128 << 10, 2048, ER_4K) },
     { INFO("n25q128",     0x20ba18,      0,  64 << 10, 256, 0) },
     { INFO("n25q256a",    0x20ba19,      0,  64 << 10, 512, ER_4K) },
     { INFO("n25q512a",    0x20ba20,      0,  64 << 10, 1024, ER_4K) },
