01eadc53cd8d39c50f10d55dd711b443e3dffe5e
Francisco Iglesias <francisco.iglesias@xilinx.com>
2021-06-17 18:20:46 +0100

    arm: cpu: Add GIC and generic timer support into the cortex-R52 PRF1
    Uncomment and add in the GIC and generic timer support into the cortex-R52
    PRF1 register (and also add the generic timers support into the internal
    CPU features tracking).
    
    Signed-off-by: Francisco Iglesias <francisco.iglesias@xilinx.com>
    Message-Id: <20210617172046.22868-3-francisco.iglesias@xilinx.com>
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- target/arm/cpu_tcg.c
+++ target/arm/cpu_tcg.c
@@ -795,10 +795,7 @@ static void cortex_r52_initfn(Object *obj)
     ARMCPU *cpu = ARM_CPU(obj);
 
     set_feature(&cpu->env, ARM_FEATURE_V8);
-/*
- * Needs DTS & GICv2 changes.
- *    set_feature(&cpu->env, ARM_FEATURE_GENERIC_TIMER);
- */
+    set_feature(&cpu->env, ARM_FEATURE_GENERIC_TIMER);
     set_feature(&cpu->env, ARM_FEATURE_MPIDR);
     set_feature(&cpu->env, ARM_FEATURE_EL2);
     set_feature(&cpu->env, ARM_FEATURE_PMSA);
@@ -806,9 +803,7 @@ static void cortex_r52_initfn(Object *obj)
     cpu->pmsav7_dregion = 16;
     cpu->midr = 0x411fd132; /* r1p2 */
     cpu->isar.id_pfr0 = 0x0131;
-    /* FIXME: Update for GICv3 and Generic timers.  */
-    /* cpu->id_pfr1 = 0x10111001; */
-    cpu->isar.id_pfr1 = 0x00101001;
+    cpu->isar.id_pfr1 = 0x10111001;
     cpu->isar.id_dfr0 = 0x010400;
     cpu->id_afr0 = 0x0;
     cpu->isar.id_mmfr0 = 0x00211040;
