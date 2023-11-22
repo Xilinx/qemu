9ee1989d9fe63658ed9797bd3551d112419a1a1f
Frederic Konrad <fkonrad@amd.com>
2023-11-22 15:00:58 +0000

    sss: implements multi target
    This implements multi-target in the Secure Stream Switch.
    
    Limitation:
      * The targets get the data chunk by chunk, so if one of the target stops
        accepting data in the last chunk, the other target might still get EOP.
    
    Signed-off-by: Frederic Konrad <fkonrad@amd.com>
    Signed-off-by: Francisco Iglesias <francisco.iglesias@amd.com>

--- hw/misc/sss.c
+++ hw/misc/sss.c
@@ -61,17 +61,24 @@ sss_lookup_rx_remote(SSSBase *s, SSSStream *ss)
     return ret;
 }
 
+/**
+ * sss_lookup_tx_remote:
+ *
+ * Check for TX remotes connected to @rx_remote by the switch.  Since the
+ * initiator can reach multiple targets, the first available target after
+ * @start will be returned.
+ */
 static inline int
-sss_lookup_tx_remote(SSSBase *s, int rx_remote)
+sss_lookup_tx_remote(SSSBase *s, int rx_remote, int start)
 {
     uint32_t enc;
+    int ret;
+
     if (rx_remote == NOT_REMOTE(s)) {
         return NOT_REMOTE(s);
     }
 
-    int ret;
-
-    for (ret = 0; ret < NOT_REMOTE(s); ++ret) {
+    for (ret = start; ret < NOT_REMOTE(s); ++ret) {
         if (s->r_sss_shifts[ret] == -1) {
             /* This unit has no input. Ignore it.  */
             continue;
@@ -86,23 +93,72 @@ sss_lookup_tx_remote(SSSBase *s, int rx_remote)
                     ret : NOT_REMOTE(s);
 }
 
-static bool
-sss_stream_can_push(StreamSink *obj, StreamCanPushNotifyFn notify,
-                                void *notify_opaque)
+static inline size_t sss_num_tx_remote(SSSBase *s, int rx_remote)
+{
+    uint32_t enc;
+    size_t ret = 0;
+    size_t i;
+
+    for (i = 0; i < NOT_REMOTE(s); i++) {
+        if (s->r_sss_shifts[i] == -1) {
+            /* This unit has no input. Ignore it.  */
+            continue;
+        }
+
+        enc = s->get_sss_regfield(s, i);
+        if (s->r_sss_encodings[rx_remote] == enc) {
+            ret++;
+        }
+    }
+
+    return ret;
+}
+
+static void sss_stream_abort(StreamSink *obj)
 {
     SSSStream *ss = SSS_STREAM(obj);
     SSSBase *s = SSS_BASE(ss->sss);
     int rx = sss_lookup_rx_remote(s, ss);
-    int tx = sss_lookup_tx_remote(s, rx);
 
-    if (tx != NOT_REMOTE(s) && s->tx_devs[tx] &&
-            stream_can_push(s->tx_devs[tx], notify, notify_opaque)) {
-        return true;
+    /* Basically clear the packet if there are pending.  */
+    memset(&s->pending_transactions[rx], 0, sizeof(SSSPendingTransaction));
+}
+
+static bool
+sss_stream_can_push(StreamSink *obj, StreamCanPushNotifyFn notify,
+                    void *notify_opaque)
+{
+    SSSStream *ss = SSS_STREAM(obj);
+    SSSBase *s = SSS_BASE(ss->sss);
+    /* Find the initiator ID for that StreamSink.  */
+    int rx = sss_lookup_rx_remote(s, ss);
+    int tx = 0;
+    bool ret = false;
+
+    tx = sss_lookup_tx_remote(s, rx, 0);
+    while (tx != NOT_REMOTE(s) && (s->tx_devs[tx])) {
+        ret = true;
+        /*
+         * If there is a pending transaction running, the number of data
+         * might be not synchronised between all the targets, some of them
+         * might already got the complete packet, and they might return false
+         * here.  Return true for those target port.
+         */
+        if ((!s->pending_transactions[rx].active
+            || s->pending_transactions[rx].remaining[tx])
+            && (!stream_can_push(s->tx_devs[tx], notify, notify_opaque))) {
+            ret = false;
+            break;
+        }
+
+        /* Check for the next target port id.  */
+        tx = sss_lookup_tx_remote(s, rx, tx + 1);
     }
 
     s->notifys[rx] = notify;
     s->notify_opaques[rx] = notify_opaque;
-    return false;
+
+    return ret;
 }
 
 static size_t sss_stream_push(StreamSink *obj, uint8_t *buf,
@@ -111,10 +167,125 @@ static size_t sss_stream_push(StreamSink *obj, uint8_t *buf,
     SSSStream *ss = SSS_STREAM(obj);
     SSSBase *s = SSS_BASE(ss->sss);
     int rx = sss_lookup_rx_remote(s, ss);
-    int tx = sss_lookup_tx_remote(s, rx);
+    int tx;
+    int tx_count = sss_num_tx_remote(s, rx);
+    size_t remaining = len;
+    uint8_t chunk[MULTI_BUF_SIZE];
+    size_t out_len;
+    bool out_eop = false;
+    size_t consumed;
 
-    return (tx != NOT_REMOTE(s)) ?
+    if (s->pending_transactions[rx].active) {
+        /*
+         * Check if there is already a transaction pending, in which case it
+         * needs to be flushed before continuing.  The beggining of the buf in
+         * the transaction is what has been copied in the data field.
+         */
+        bool transfer_completed = true;
+
+        for (tx = 0; tx < MAX_REMOTE; tx++) {
+            if (s->pending_transactions[rx].remaining[tx]) {
+                /*
+                 * In case the mapping changed and the remote target is not
+                 * accessible to the source: drop the data.
+                 */
+                if (sss_lookup_tx_remote(s, rx, tx) != tx) {
+                    s->pending_transactions[rx].remaining[tx] = 0;
+                    continue;
+                }
+
+                /* Some data remains for tx, try to flush them.  */
+                out_len = MIN(remaining,
+                              s->pending_transactions[rx].remaining[tx]);
+                g_assert(out_len <= MULTI_BUF_SIZE);
+                memcpy(chunk,
+                       s->pending_transactions[rx].data
+                       + s->pending_transactions[rx].data_len
+                       - s->pending_transactions[rx].remaining[tx],
+                       out_len);
+                consumed = stream_push(s->tx_devs[tx], chunk, out_len, out_eop);
+                s->pending_transactions[rx].remaining[tx] -= consumed;
+                if (s->pending_transactions[rx].remaining[tx]) {
+                    /* There are still remaining data unfortunately.  */
+                    transfer_completed = false;
+                }
+            }
+        }
+
+        if (transfer_completed) {
+            /*
+             * All the targets got the data.  Remove the active flag, and
+             * resume the normal operations.
+             */
+            remaining -= s->pending_transactions[rx].data_len;
+            s->pending_transactions[rx].active = false;
+        } else {
+            /* There are still data..  */
+            return 0;
+        }
+    }
+
+    switch (tx_count) {
+    case 0:
+        return 0;
+    case 1:
+        /*
+         * The simple case where we have only one remote.  Let's keep it simple
+         * and pass the target the whole data.
+         */
+        tx = sss_lookup_tx_remote(s, rx, 0);
+        return (tx != NOT_REMOTE(s)) ?
             stream_push(s->tx_devs[tx], buf, len, eop) : 0;
+    default:
+        break;
+    }
+
+    /*
+     * Handle multiple targets.  The risk here is that all the targets don't
+     * accept the same amount of data.  In that case the transfer should stop
+     * and wait that all the targets can have data again.
+     *
+     * So pass the data chunk by chunk and keep a copy.  If any of the target
+     * is refusing the data, stops and store the status.
+     */
+    while (remaining && !s->pending_transactions[rx].active) {
+        tx = sss_lookup_tx_remote(s, rx, 0);
+        /* Compute the length for the next transaction.  */
+        out_len = MIN(remaining, MULTI_BUF_SIZE);
+        /* For the last chunk and if input has EOP, send an EOP.  */
+        out_eop = ((remaining - out_len) == 0) ? eop : false;
+
+        while (tx != NOT_REMOTE(s) && (s->tx_devs[tx])) {
+            /* This has to be done, because some target corrupt the data.  */
+            memcpy(chunk, &buf[len - remaining], out_len);
+            consumed = stream_push(s->tx_devs[tx], chunk, out_len, out_eop);
+            if (consumed != out_len) {
+                /* The target didn't accept the complete transaction.  */
+                if (!s->pending_transactions[rx].active) {
+                    memset(&s->pending_transactions[rx], 0,
+                           sizeof(SSSPendingTransaction));
+                    s->pending_transactions[rx].active = true;
+                    memcpy(s->pending_transactions[rx].data,
+                           &buf[len - remaining],
+                           out_len);
+                    s->pending_transactions[rx].data_len = out_len;
+                }
+                s->pending_transactions[rx].remaining[tx] = out_len - consumed;
+            }
+            /* Check for the next target port id.  */
+            tx = sss_lookup_tx_remote(s, rx, tx + 1);
+        }
+
+        /*
+         * Always assume that the last block hasn't been consumed in case the
+         * last block didn't go through.
+         */
+        remaining = s->pending_transactions[rx].active
+            ? remaining
+            : remaining - out_len;
+    }
+
+    return len - remaining;
 }
 
 static void sss_stream_class_init(ObjectClass *klass, void *data)
@@ -123,6 +294,7 @@ static void sss_stream_class_init(ObjectClass *klass, void *data)
 
     ssc->push = sss_stream_push;
     ssc->can_push = sss_stream_can_push;
+    ssc->abort = sss_stream_abort;
 }
 
 static const TypeInfo sss_info = {
--- include/hw/misc/sss.h
+++ include/hw/misc/sss.h
@@ -17,6 +17,7 @@
     (s->num_remotes)
 
 #define MAX_REMOTE 16
+#define MULTI_BUF_SIZE 1024
 
 typedef struct SSSBase SSSBase;
 typedef struct SSSStream SSSStream;
@@ -25,6 +26,26 @@ struct SSSStream {
     DeviceState parent_obj;
 
     SSSBase *sss;
+};
+
+typedef struct SSSPendingTransaction SSSPendingTransaction;
+/**
+ * struct SSSPendingTransaction:
+ *
+ * This structure describes an unfinished transaction.  There should be one
+ * per RX channel.
+ *
+ * @activated: There is a pending transaction on the associated RX channel.
+ * @remaining: The amount of bytes which needs to be copied for the given TX
+ *             channel.
+ * @data:      The chunk of memory which needs to be transfered.
+ * @data_len:  The size of the data.
+ */
+struct SSSPendingTransaction {
+    bool active;
+    size_t remaining[MAX_REMOTE];
+    uint8_t data[MULTI_BUF_SIZE];
+    size_t data_len;
 };
 
 struct SSSBase {
@@ -42,6 +63,8 @@ struct SSSBase {
     const uint8_t *r_sss_encodings;
     const uint8_t (*sss_cfg_mapping)[MAX_REMOTE];
     uint8_t num_remotes;
+
+    SSSPendingTransaction pending_transactions[MAX_REMOTE];
 };
 
 void sss_notify_all(SSSBase *s);
